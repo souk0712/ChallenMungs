57d671063fd7ef19f21147bbb56e00842174f6d6
choijiwon <gwonwon1201@gmail.com>
2023-03-15 10:40:31 +0900

    [SETTING] 백엔드 프로젝트 생성

---
+++ src/main/java/com/ssafy/ChallenMungs/ChallenMungsApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5eea67a0ca9828dd2320fb0c97042ac40340ea79677aabea080654d910d7760d
+size 327
---
+++ src/main/java/com/ssafy/ChallenMungs/Test/controller/testController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:94e2676ad83b4cdc3dd14123fbad486baddf3d5aa2f11ebae78b76d06ca1bd16
+size 565
