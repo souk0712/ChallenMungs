35504b07863cd307fa39ef367dc4f68aaf8bdf32
이동하 <siganshoyou@gmail.com>
2023-03-20 17:59:44 +0900

    일단 아이디 있는지 없는지 찾았어요
    Former-commit-id: 15084c329a8f9c5901b06484d3fa48a9bef1122f

---
+++ src/main/java/com/ssafy/ChallenMungs/User/User.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:35d98acf6623a0c592d55cefbdc7ad51366afe8c9a1ef7e6564a82b956400a0b
+size 664
---
+++ src/main/java/com/ssafy/ChallenMungs/User/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.ChallenMungs.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User> {
+    int countByLoginId(String str);
+}
