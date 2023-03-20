ed974c7296c4973242fe3fb54d5522b763bf9184
choijiwon <gwonwon1201@gmail.com>
2023-03-20 20:53:06 +0900

    [SETTING] querydsl 설정

---
+++ src/main/java/com/ssafy/ChallenMungs/Test/repository/QuerydslRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab250bc8a502f374124dc338edebe72a86f6b5d483234e1990aa1797aaa8b760
+size 188
--- src/main/java/com/ssafy/ChallenMungs/Test/service/TestService.java
+++ src/main/java/com/ssafy/ChallenMungs/Test/service/TestService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:806a1b63bf8eabb252d1d6bdf9fcf275367fee9163cc9f6d96179ecc470dd73f
-size 212
+oid sha256:903da8cc9fc2b53afc9721603418729398d02ddea31cde8c52d411c7cef5d588
+size 245
