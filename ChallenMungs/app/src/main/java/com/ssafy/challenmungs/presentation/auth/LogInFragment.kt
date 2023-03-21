defc7b193629021baf648a061c1381c1265a28ee
dobbi030 <dmstjs7047@hanmail.net>
2023-03-21 10:43:16 +0900

    [FEAT] kakao login layout 구현
    [FEAT] kakao login layout 구현
    
    
    Former-commit-id: 0824ff217f9c04bfe837060ed5319c916f940bea

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/LogInFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.challenmungs.presentation.auth
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentLogInBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class LogInFragment : BaseFragment<FragmentLogInBinding>(R.layout.fragment_log_in) {
+
+    override fun initView() {
+    }
+}
