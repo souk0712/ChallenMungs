cee40e7fc704b5775c8028886977f195d709b6b4
kimbugyeong <sa01023@naver.com>
2023-03-21 17:46:51 +0900

    [FEAT]회원탈태
    Former-commit-id: 0e722932ac24032a69df3c0c6e7889fd07c1faf9

--- src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
+++ src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fba2547b78c3d089a546c8fe8cee952b54e9597eb9b8c51250cdb4ac04729bd
-size 8107
+oid sha256:cf1f95a971ad9607b5ab1ecfdf8454f21f8654c8896cf8c3ff0db03a0c65dc82
+size 7989
