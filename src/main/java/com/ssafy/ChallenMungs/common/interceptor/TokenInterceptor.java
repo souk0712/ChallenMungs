a6c5a10deb57753e18be8dd260720086705b7169
이동하 <siganshoyou@gmail.com>
2023-03-21 17:22:00 +0900

    [FEAT]filecontroller받아오기
    Former-commit-id: e1e12a3afe4f2e56aee5f3881da57e43616d56b1

--- src/main/java/com/ssafy/ChallenMungs/common/interceptor/TokenInterceptor.java
+++ src/main/java/com/ssafy/ChallenMungs/common/interceptor/TokenInterceptor.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39982e4dcbbf76cc6501a2534f86b028d3e15aaeefc625b71ad02419dc0e11e2
-size 976
+oid sha256:04615b51833f61745d0765462e02844898db843486deabc4987caee8b6f53c2a
+size 1045
