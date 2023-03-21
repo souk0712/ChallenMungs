6437400620eb40f54d9ac50686b20333ce1e0db0
choijiwon <gwonwon1201@gmail.com>
2023-03-21 16:59:46 +0900

    [FEAT] 함수 이름 수정 S08P22D210-184
    Former-commit-id: 26b8284d77b6094ab0f258952f06852cd07d38b6

--- src/main/java/com/ssafy/ChallenMungs/image/controller/FileController.java
+++ src/main/java/com/ssafy/ChallenMungs/image/controller/FileController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ad033016f5c3e78aa4ba54651d8ff8262452db269bdb009461c8db075e6edb3
-size 1187
+oid sha256:250ca3e72f1af6f3e49c5a527bec36f659bec6737b9ef9ff786d5fd521f6d8d2
+size 1198
