a8583fc9288e01e188e9654d5ea87b43b40b023c
이동하 <siganshoyou@gmail.com>
2023-03-22 08:29:27 +0900

    [feat]저장성공
    Former-commit-id: df70e2c978823a9e2c93f534aaa4d7dfd6be58ac

--- src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32218b60f97c609dd84cb332279cc63a62ebdc085e336de809e77278b01dba9e
-size 457
+oid sha256:4e2ab3ba7422a234032736c9f006c526aedd8b8355e41d59e347801b7b18afc0
+size 517
