99e50f864fa77af1a9bc13377d9828c7164a75f5
이아름 <souk0712@naver.com>
2023-03-22 02:23:15 +0900

    [CHORE] 마이페이지 layout 코드 컨벤션에 맞춰 수정
    Former-commit-id: 346d7a20170cb49037de1905cb64be8d6e2457e8

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyPageFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyPageFragment.kt
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0f72316ea1c5232e56027de9cb674110583dfd1170118ff516937330d4675e27
-size 594
+package com.ssafy.challenmungs.presentation.mypage
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentMyPageBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    override fun initView() {
+
+    }
+}
