1e8c43108943b5a226bd0f94d13c7bf0cf9d2cbf
dobbi030 <dmstjs7047@hanmail.net>
2023-03-22 01:50:52 +0900

    [FEAT] 하단 메뉴 layout 구현
    Former-commit-id: da2a63fe3fa6f6ae54b276a56e573a0b127747ec

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/Extension.kt
@@ -0,0 +1,19 @@
+package com.ssafy.challenmungs.common.util
+
+import android.content.Context
+
+fun Int.dp(context: Context): Int {
+    return (this / context.resources.displayMetrics.density).toInt()
+}
+
+fun Int.px(context: Context): Int {
+    return (this * context.resources.displayMetrics.density).toInt()
+}
+
+fun getDeviceWidthPx(context: Context): Int {
+    return context.resources.displayMetrics.widthPixels
+}
+
+fun getDeviceHeightPx(context: Context): Int {
+    return context.resources.displayMetrics.heightPixels
+}
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/MainActivity.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/MainActivity.kt
@@ -1,12 +1,3 @@
-package com.ssafy.challenmungs.presentation
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.ssafy.challenmungs.R
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:d7b11af2fab29b2ce3d246b687a0f467bd7f52b133f8cbddc656beddb67fb578
+size 2962
