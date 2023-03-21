d81398af385c77d8d7dff702857f39b4388a8b89
이아름 <souk0712@naver.com>
2023-03-22 02:05:20 +0900

    [FEAT] 내지갑 layout 구현
    Former-commit-id: 3935b46eadd70dfe61751fccde59b272269d562a

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/MyWallet.kt
@@ -0,0 +1,12 @@
+package com.ssafy.challenmungs.presentation
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentMyWalletBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class MyWallet : BaseFragment<FragmentMyWalletBinding>(R.layout.fragment_my_wallet) {
+
+    override fun initView() {
+
+    }
+}
