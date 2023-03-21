03c143139a7df796530ad26379905658020f9a4f
이동하 <siganshoyou@gmail.com>
2023-03-22 07:27:55 +0900

    [FEAT]파라미터예시
    Former-commit-id: bd2f9f165f6a230b928ce71c6c6c3489317471a0

--- src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
+++ src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38864285ec6e0c5c9fb5e82b31c4881be18b36ddeeb6c4f40479f0a106edfccf
-size 8854
+oid sha256:c9ae5639b51675ce27f2ead2ac8ec05df6a7d4e2099b69dba94e8a1d292a5e65
+size 9409
--- src/main/java/com/ssafy/ChallenMungs/user/dto/Res2.java
+++ src/main/java/com/ssafy/ChallenMungs/user/dto/Res2.java
@@ -3,12 +3,13 @@ package com.ssafy.ChallenMungs.user.dto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class res1 {
-    @ApiModelProperty(value = "사용자의 이름", example = "steve", required = true)
+public class Res2 {
+    @ApiModelProperty(value = "어떤값이 들어오는지 설명", example = "테스트 값", required = true)
     private String result;
 }
