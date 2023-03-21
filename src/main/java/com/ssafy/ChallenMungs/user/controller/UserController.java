ac21c9ce7def6dac16fbc93b33a9853bb5f16cf7
kimbugyeong <sa01023@naver.com>
2023-03-21 17:00:19 +0900

    [FEAT]회원탈퇴(#171)
    Former-commit-id: 1f1fbdcb1d433badbe5e5d9fad10a8a8718f36a8

--- src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
+++ src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22ff944ebd6092e6305962a8376113a7b18836734423e23e1088b56ef8c1ea4c
-size 7463
+oid sha256:7fba2547b78c3d089a546c8fe8cee952b54e9597eb9b8c51250cdb4ac04729bd
+size 8107
--- src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 public interface UserRepository extends JpaRepository<User, Long>, QuerydslPredicateExecutor<User> {
     int countByLoginId(String str);
     User findUserByLoginId(String loginId);
+    void deleteUserByLoginId(String loginId);
 }
