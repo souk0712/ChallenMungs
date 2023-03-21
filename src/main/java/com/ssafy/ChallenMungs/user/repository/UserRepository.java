262c199d826ee8fe63af42e2e24976569f4ec6ab
이동하 <siganshoyou@gmail.com>
2023-03-21 14:37:40 +0900

    [FEAT]테스트
    Former-commit-id: 1d2f609bafad20ff2d78b034975f936c89222839

--- src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a0e17e281178d343672f0ae09f0fd9263b9e8cc236cb07b6eac037610a3549f
-size 367
+oid sha256:0ebc088661075b6f22b786fe01da02dd2f5339acd708934d94b86a917f2d0543
+size 411
