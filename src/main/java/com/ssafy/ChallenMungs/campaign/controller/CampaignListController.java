95968721e64122652401f0fc205479baa3e62d50
kimbugyeong <sa01023@naver.com>
2023-03-22 15:17:44 +0900

    [FEAT]중복체크
    Former-commit-id: 32b09f0831270871bab4edabd3a265f0d2fd2ef3

--- src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignListController.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignListController.java
@@ -1,8 +1,3 @@
-package com.ssafy.ChallenMungs.campaign.controller;
-
-public class CampaignListController {
-    //todo 각종 옵션에 따라 캠페인 리스트를 반환하는 api
-    //todo 보호소 화면에서 자신의 캠페인 리스트를 보여주는 api
-    //2개가 필수 구현이고 나머지(내가 좋아요한 캠페인 등)는 적당히 추가해주세요
-    
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:4c4bc36477e1ce512f6751534f256009a9209cae842bd43d9b98c8602b417dcf
+size 368
