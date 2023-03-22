316d0a9036a877abff2ce7a94a08ef7794870134
kimbugyeong <sa01023@naver.com>
2023-03-22 10:59:58 +0900

    [FIX]회원PK
    Former-commit-id: 569c22b2e42090f264e8b6f1a3eb2c036876d3e9

--- src/main/java/com/ssafy/ChallenMungs/user/entity/User.java
+++ src/main/java/com/ssafy/ChallenMungs/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:987577bf8afce7ddd09a750e8566dcdc300ebeae9da72868829f716db107aada
-size 672
+oid sha256:7fd713c725574605b54da775c03a20954d63015233c83de9673e9daf416a1754
+size 691
