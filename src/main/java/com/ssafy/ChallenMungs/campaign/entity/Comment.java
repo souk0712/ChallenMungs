7315d28edcb75e916a565403869597f9c438a2ae
choijiwon <gwonwon1201@gmail.com>
2023-03-22 14:35:54 +0900

    [FEAT] 필요없는 칼럼 지움 S08P22D210-185
    Former-commit-id: a5db8a1fbb9c64a01a61cb039456a83ba6d48783

--- src/main/java/com/ssafy/ChallenMungs/campaign/entity/Comment.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/entity/Comment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84f832cad076941b929ffcd5836fc20cedbd5cc61face9dd21e26b30598af062
-size 530
+oid sha256:ef8e1968519cda5863627c505fd9e8bbf32691460408bd81fbf18997b80fc6e2
+size 478
