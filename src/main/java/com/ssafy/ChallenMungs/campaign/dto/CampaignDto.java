d6754d232b518ab1b1bcdd05a32fd89ae66512c6
변윤경 <forever4410@gmail.com>
2023-03-22 17:30:47 +0900

    [FEAT] 캠페인 조회 Controller 추가
    Former-commit-id: fb521779bd8ad7236972a57cf938ef9634e5dc3f

--- src/main/java/com/ssafy/ChallenMungs/campaign/dto/CampaignDto.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/dto/CampaignDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d09a38770bc7e51796dccf651bd96f9f6d040fb8e36a45945998fb06f1f1f3d
-size 878
+oid sha256:f273f3b7b98255a668beb0e105527ac0ac95f738d605909b591f1ba9bef8f4f5
+size 899
