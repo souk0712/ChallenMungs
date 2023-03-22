aca04e303d63417ebef3445aa89dcf4838a67720
choijiwon <gwonwon1201@gmail.com>
2023-03-22 09:26:43 +0900

    [REFACTOR] todo 정리 S08P22D210-185
    Former-commit-id: 3fdc2d48468ec4e3de97ac5147c86214c055b8e4

--- src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignListController.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignListController.java
@@ -1,4 +1,3 @@
-package com.ssafy.ChallenMungs.campaign.controller;
-
-public class CampaignListController {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:68f48741b6fd4789b7d048cce20322c7325f2b7d4f93aa974719e58077c6478b
+size 363
