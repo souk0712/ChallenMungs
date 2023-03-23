e55514410de1b00f3eb2a82ab323d8796a3576f1
dobbi030 <dmstjs7047@hanmail.net>
2023-03-23 18:00:36 +0900

    [FEAT] 로그인 후 화면 전환
    Former-commit-id: ff39a0e064daaa40e7129adb61a6529a0aabf0f2

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/AuthViewModel.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/AuthViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac70ac01a3d7f38c0218f2b72b9ae4fc073c9e9c153bb4899a473277fc819559
-size 2072
+oid sha256:1c125442b5179a8f4b5b93a99d6136ddfc3f90741e746c3fb9049443efd685bb
+size 2123
