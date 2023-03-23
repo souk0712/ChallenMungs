fa1a412c9776a6e7085b5767ef260e7663274ee8
dobbi030 <dmstjs7047@hanmail.net>
2023-03-23 18:00:36 +0900

    [FEAT] 로그인 후 화면 전환
    Former-commit-id: 4c7215dc3ef12944ba63120ed32a8b4682976f7c

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/LogInFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/LogInFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d845af64ad482fabf4dbd8e3591ba0faedb6b34f97222cfccf2460927d07f6e4
-size 2909
+oid sha256:daa5c82e4b5cbd0ee2fa245c3bbc80881f9315d968fb773433863b4bd2515b3e
+size 3599
