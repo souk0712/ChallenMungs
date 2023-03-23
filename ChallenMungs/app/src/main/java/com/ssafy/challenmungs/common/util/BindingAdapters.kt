e3e34342146002f236752ff1c5024283aed516a3
이아름 <souk0712@naver.com>
2023-03-23 22:10:45 +0900

    [FEAT] 프로필 수정 layout 구현
    Former-commit-id: 17bd560c116cda607b55eee2729b0bf858b1e849

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/BindingAdapters.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d744bc57715b6d42c52bfd33ad1a0f625191960b1a5fc866efeb03e86f505edf
+size 546
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/EditProfileFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/EditProfileFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f936e2bf2b765cf32e15c4dd23380d3ee76a75632d4957fcefd91d3e5d8fd0eb
-size 639
+oid sha256:f4292d9aaa764cb904b80d4f16f018807614f9e0ac7d463fb84528c9f87b948b
+size 883
