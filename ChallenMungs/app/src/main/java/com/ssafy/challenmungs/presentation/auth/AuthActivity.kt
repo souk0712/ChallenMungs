f78d312bda7f871ceff5898bd4e33dc67c0ef34c
dobbi030 <dmstjs7047@hanmail.net>
2023-03-23 17:30:15 +0900

    [FIX] accessToken이 null일때도 유효성을 검사하여 api 통신 오류가 발생하던 문제 해결
    Former-commit-id: f19740b61644fe78f971c3c6ae551cbb35f04f0b

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/AuthActivity.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/AuthActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b774c8a11c96c543eccac560e8f28fa9c894fbd634337e576063a8c49ceaa77f
-size 1365
+oid sha256:ff294d1a28f5b5ab4c5912e9f3edd009ae4e6b88e55cd2e0768efd9f3684638f
+size 1397
