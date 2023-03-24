408a35041f041a9101148c19fa2c513c007693b4
kimbugyeong <sa01023@naver.com>
2023-03-24 15:31:32 +0900

    [FIX]merge_issue
    Former-commit-id: fd679e2129b6b3ed0d2514caf590ba3ace992646

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/entity/Challenge.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/entity/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:380596420644440dfb34ad1aa0c7c5f6dec45d53a1db489960ec224a180b067f
-size 1708
+oid sha256:7954f8c0ae665c91da3e1721af14172fc33868c7f994ad5ddee2e7e3d2a64041
+size 1641
