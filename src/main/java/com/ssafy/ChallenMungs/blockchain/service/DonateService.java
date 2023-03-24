458959ddcf8395861480f98255bd3d94cf473ca4
kimbugyeong <sa01023@naver.com>
2023-03-24 18:00:31 +0900

    [FEAT]일반챌린지참가자저장
    Former-commit-id: 96c880b412ba606a5ef53ed3c0eb1e45594262d7

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/DonateService.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/DonateService.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d37873988e2f51c4e5d773d7c63a56504a131793461f688a7e002c1daa53657f
-size 87
+package com.ssafy.ChallenMungs.blockchain.service;
+
+public interface DonateService {
+}
---
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/repository/GeneralParticipantRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e21411a5a8f108be98300bc5bd85a1bc1e5d67c6b874318f205c4597e16b36ef
+size 521
