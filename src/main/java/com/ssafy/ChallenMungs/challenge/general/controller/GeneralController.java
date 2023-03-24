d1d991cac7a60c9d70cd474bd174bc181c8a3aba
kimbugyeong <sa01023@naver.com>
2023-03-24 15:28:52 +0900

    [FIX]일반챌린지타입설정
    Former-commit-id: 3cca40e42f348a0aea07ea232e478798c758b68a

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e940a03d9d567a37feeb653b5e3d8bde3857ac1028dd93d96fd3c6a3cc80e27d
-size 2530
+oid sha256:8d466d991269e7c23e8f668c463fb3efed62a98119638d3a43472ea670ffac9b
+size 2533
