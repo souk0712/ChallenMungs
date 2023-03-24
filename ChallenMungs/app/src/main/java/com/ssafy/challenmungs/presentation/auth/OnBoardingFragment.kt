25322822dfeca92d98e475ce7a5e25757a6d2241
dobbi030 <dmstjs7047@hanmail.net>
2023-03-24 10:44:34 +0900

    [CHORE] AuthActivity에서 자동 로그인 성공하면 MainActivity로 이동하는 방식 변경
    intent로 이동하는 것이 아닌 MainActivity를 navigation_auth의 자식으로 추가하여 startDestination을 설정하는 방식으로 변경.
    -> 자동로그인시 로그인 화면이 보이지 않고 바로 MainActivity로 이동하는 효과를 기대했으나 여전히 LogInFragment가 보임. 따라서 추후 SplashActivity를 따로 생성하여 해당 액티비티에서 자동 로그인을 하고 로그인 결과에 다라 액티비티 이동을 다르게 하는것으로 바꿀 예정
    
    
    Former-commit-id: b580c4370d85eb46f9818f30d0c507009eed5291

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7fb51d742f6ca6a34307f17eb50ce93f153b1890ca22c4a9ab9780591751c87
-size 1477
+oid sha256:d9f295423bae242a30aa45dfc95c4db47994d33d67e68da3b823c2e1d5c236b3
+size 1597
