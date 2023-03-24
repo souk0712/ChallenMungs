e5482ff33f6faba6c5ff1c08723286c209f710fe
dobbi030 <dmstjs7047@hanmail.net>
2023-03-24 10:41:14 +0900

    [CHORE] MemberViewModel의 getMemberInfo 함수를 suspend 함수로 변경
    Former-commit-id: 7163b336284fae9b0a97422f19b66436ee5d5e7a

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/MemberViewModel.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/MemberViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad546cc48ca6191775d265ebc5443ca2a7e6833ffcbfc483af406eb4837f54c3
-size 1090
+oid sha256:d274f6dcc570a6ab76738787804d5f93ff85b2bed64c3496bb29a6f9fe6e8433
+size 1221
