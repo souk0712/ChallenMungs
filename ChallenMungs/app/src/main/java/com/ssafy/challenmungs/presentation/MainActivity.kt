78947ed8052301467711517a79db0e6bf8203316
dobbi030 <dmstjs7047@hanmail.net>
2023-03-24 13:19:44 +0900

    [CHORE] 불필요한 코드 삭제
    Former-commit-id: 149142fe21be8c09b4798a64f789afa3d7fb477b

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/MainActivity.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/MainActivity.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17c4634927feef50c93dfa87819a40437b15f7f7546c1db46d465472f792e6dc
-size 3112
+oid sha256:048a32619108c6eb44ca02ce72fd9051f79f68658bf78b47945c7f3d2f881faa
+size 2795
