8e9252fc527356c3ef9b882ec55274aafd4a834a
dobbi030 <dmstjs7047@hanmail.net>
2023-03-24 10:45:56 +0900

    [FIX] api 통신 헤더 변경
    Authorization의 value에 bearer가 붙어 통신되지 않던 문제 해결
    
    
    Former-commit-id: 6e2666cc2815e0c2fd45d3f1f279b1e3ea4315c3

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/AuthInterceptor.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/AuthInterceptor.kt
@@ -8,7 +8,7 @@ class AuthInterceptor(private val preferences: SharedPreferences) : Interceptor 
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "bearer " + preferences.accessToken).build()
+            .addHeader("Authorization", preferences.accessToken.toString()).build()
 
         return chain.proceed(request)
     }
