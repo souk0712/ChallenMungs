48a9ad18f0c4b9ae1a777a87d4c4b7fad45c2e3d
kimbugyeong <sa01023@naver.com>
2023-03-24 16:04:54 +0900

    [FEAT]일반챌린지조회
    Former-commit-id: a22d51cd5ea1f3d4153b7c15123ca2efcb44e34f

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e940a03d9d567a37feeb653b5e3d8bde3857ac1028dd93d96fd3c6a3cc80e27d
-size 2530
+oid sha256:1d95de63423035ab3eb76cae74523548bb42a5b9b44373f8dd7d28ca02f1d5af
+size 2985
