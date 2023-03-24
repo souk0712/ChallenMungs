81fa92624170306a920f7a1a10054b69641d3d00
choijiwon <gwonwon1201@gmail.com>
2023-03-24 09:58:53 +0900

    [FEAT] 캠페인 조아요 수 함수 생성
    Former-commit-id: 5fa9bb77fab7c6ee1982d07be8e18484cac68507

--- src/main/java/com/ssafy/ChallenMungs/campaign/entity/Campaign.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/entity/Campaign.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6308d364ae951a3af6cc1b8aaacd7606908b374a3e6359a48071fa206c491c16
-size 1533
+oid sha256:bcebfa22d91e1281991edb2c14c4b77653ca2547280b630019035974c3fe2e96
+size 1547
