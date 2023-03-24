ec765b3951329987d76ac6271ecc044643e93dbd
kimbugyeong <sa01023@naver.com>
2023-03-24 14:11:44 +0900

    [FIX]merge_issue
    Former-commit-id: 7adbfead245725f762bad225bcef797227f51c85

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/service/GeneralBoardService.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8152202061eae45573a8a1eeb9e51b2dbc5d64f03c0fb4d61ed70628065b24c5
-size 735
---
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/service/GeneralService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64555edab0b1970ed65fe4c8d6266edb6efffedaa7a4c0860193d16c85a2798d
+size 730
