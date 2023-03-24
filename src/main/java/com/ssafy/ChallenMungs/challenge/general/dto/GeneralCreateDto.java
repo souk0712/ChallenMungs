773e2d6e1702c6d3e97b8eed02cf9180ef36c4a7
kimbugyeong <sa01023@naver.com>
2023-03-24 09:50:25 +0900

    [FIX]DB초기화설정변경
    Former-commit-id: b90bf93f17533c7282eddad5a03137013ed5d3be

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/dto/GeneralCreateDto.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/dto/GeneralCreateDto.java
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5646faa72153e52d0c75411017bc83a475e762c03b62b2eaa1c83db71b595a6f
-size 540
+package com.ssafy.ChallenMungs.challenge.general.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+//일반챌린지를 만드는데 필요한 정보들을 담은 dto 입니다.
+public class GeneralCreateDto {
+    // 제목, 시작날짜, 끝날짜, 최대인원, 현재인원, 요금
+    String title;
+    String startDate;
+    String endDate;
+    int maxParticipantCount;
+    int currentParticipantCount;
+    int entryFee;
+
+}
