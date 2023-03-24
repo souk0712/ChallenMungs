7632be1e9ee8ad5e4f1b6816b561505a2d3332ec
변윤경 <forever4410@gmail.com>
2023-03-24 16:02:34 +0900

    [FEAT] 보호소 로그인 시 캠페인 목록 조회
    Former-commit-id: 0c1f646eb3804ce68180a668defc247a9bb7fe66

--- src/main/java/com/ssafy/ChallenMungs/campaign/dto/CampaignDto.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/dto/CampaignDto.java
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f273f3b7b98255a668beb0e105527ac0ac95f738d605909b591f1ba9bef8f4f5
-size 899
+package com.ssafy.ChallenMungs.campaign.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class CampaignDto {
+    int campaignId;
+    String thumbnail;
+    String title;
+    String name;
+    int collectAmount;
+    int targetAmount;
+    int loveCount;
+
+
+}
--- src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignListServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignListServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d608c905c494dae7c49fcee4786eb10fa6101c7cdbc23fe7a180be8bde81eec4
-size 2312
+oid sha256:efcde6a4e835763c41e975091b49a52c1a28edc63a58ccf86c63bc98b75e5592
+size 2951
