14454603bbad466090b16e02ec8be49efe63e9d6
변윤경 <forever4410@gmail.com>
2023-03-24 13:54:04 +0900

    [FEAT] 지도 조회 paging 기능 추가
    Former-commit-id: 5dc1daa26cd943c0f758210cccc17469a04f41a1

--- src/main/java/com/ssafy/ChallenMungs/place/service/PlaceService.java
+++ src/main/java/com/ssafy/ChallenMungs/place/service/PlaceService.java
@@ -1,9 +1,12 @@
 package com.ssafy.ChallenMungs.place.service;
 
 import com.ssafy.ChallenMungs.place.entity.Place;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface PlaceService {
-    List<Place> getPlace(List<String> cities, String type);
+    Page<Place> getPlace(Pageable pageable, List<String> cities, String type);
+//    Page<Place> pagingList(Pageable pageable);
 }
