829df0812504a55710e26915ba9d45052c331b3d
이동하 <siganshoyou@gmail.com>
2023-03-24 15:09:13 +0900

    [FIX]데이터베이스에시간이표시되는문제해결
    Former-commit-id: 1631e4f61acc11208794abd778efe706932aad57

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/service/PanelService.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/service/PanelService.java
@@ -1,9 +1,3 @@
-package com.ssafy.ChallenMungs.challenge.panel.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PanelService {
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c381085647d3ee2bd8cdd54846873c09df44f5dc1f7b646f568de847787c7a2
+size 530
