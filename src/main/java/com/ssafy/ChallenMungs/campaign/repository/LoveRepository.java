ab6ad351036f454bbcae67cdd8c16b6b9f64bd59
choijiwon <gwonwon1201@gmail.com>
2023-03-24 09:58:53 +0900

    [FEAT] 캠페인 조아요 수 함수 생성
    Former-commit-id: d75746b1ad982e531fb819f6630bb56b8d21bc5e

--- src/main/java/com/ssafy/ChallenMungs/campaign/repository/LoveRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/repository/LoveRepository.java
@@ -1,11 +1,3 @@
-package com.ssafy.ChallenMungs.campaign.repository;
-
-import com.ssafy.ChallenMungs.campaign.entity.Campaign;
-import com.ssafy.ChallenMungs.campaign.entity.Love;
-import com.ssafy.ChallenMungs.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoveRepository extends JpaRepository<Love, Long> {
-    Love findByUserAndCampaign(User user, Campaign campaign);
-    int countByUserAndCampaign(User user, Campaign campaign);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:38f492465842e6c66bdc090630b1d83f302a012c909b902ed6974be164a222f0
+size 545
