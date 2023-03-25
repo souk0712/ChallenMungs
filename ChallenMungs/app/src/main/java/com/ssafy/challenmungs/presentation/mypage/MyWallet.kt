23528735d2e370b6a7535a631a0e7c27b1399e92
이아름 <souk0712@naver.com>
2023-03-25 15:47:31 +0900

    [FEAT] 정보 toolbar layout 구현 (#S08P22D210-153)
    Former-commit-id: 401023a7a8d15828f0900ecb879270d62931657a

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyWallet.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyWallet.kt
@@ -1,12 +1,3 @@
-package com.ssafy.challenmungs.presentation.mypage
-
-import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.databinding.FragmentMyWalletBinding
-import com.ssafy.challenmungs.presentation.base.BaseFragment
-
-class MyWallet : BaseFragment<FragmentMyWalletBinding>(R.layout.fragment_my_wallet) {
-
-    override fun initView() {
-
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:73c258bf303e434115ab6151c290a86d4583143dd840feb113fc3a5292bb5ecf
+size 337
