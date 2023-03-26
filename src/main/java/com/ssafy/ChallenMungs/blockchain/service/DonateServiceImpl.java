3b44b10b46d4fc541bac0275f834587efb6e0e7e
choijiwon <gwonwon1201@gmail.com>
2023-03-26 16:39:01 +0900

    [FEAT] 출금, 이월 기능 추가
    Former-commit-id: 09d57a4d2965c8226af80a42488aba0cc25158f3

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/DonateServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/DonateServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4638c5cee0cd9aaf699ee0f5cffaf1891080fc010ba08fa504aea8cd5c270734
-size 3213
+oid sha256:79d6d57251a76e31d64b76c2b4c3c968351d0a4c7f2f008f5566d4b1471dda32
+size 4171
