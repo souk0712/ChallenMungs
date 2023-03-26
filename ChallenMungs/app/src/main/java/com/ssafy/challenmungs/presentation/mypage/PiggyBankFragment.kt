9f4fa04385851d914c1e7a0cb1ffcdcda138a36b
이아름 <souk0712@naver.com>
2023-03-26 15:35:32 +0900

    [FEAT] 저금통 상세조회 layout 구현 (#S08P22D210-199)
    Former-commit-id: 5473b9a07eea446906ec8c40c0154a54da533bb7

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/PiggyBankFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.challenmungs.presentation.mypage
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentPiggyBankBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class PiggyBankFragment : BaseFragment<FragmentPiggyBankBinding>(R.layout.fragment_piggy_bank) {
+
+    override fun initView() {
+
+    }
+}
