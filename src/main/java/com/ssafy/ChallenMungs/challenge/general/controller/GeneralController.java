da4f16170e6bd3a82b5ed0f15ff3d4a04e5f457e
kimbugyeong <sa01023@naver.com>
2023-03-26 20:15:45 +0900

    [FIX]챌린지조회표현방식수정
    Former-commit-id: b648e737ae7e38518eeae0c84d31b92011c4d217

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:913806e1b4153bc81de5bc157d024b01a2b447b819e30cd5b0600a10299cbdd2
-size 3965
+oid sha256:3ef83d1002054f3070dd1ef35bf3daca61c3942681c6066e814b2453e33dd714
+size 4120
