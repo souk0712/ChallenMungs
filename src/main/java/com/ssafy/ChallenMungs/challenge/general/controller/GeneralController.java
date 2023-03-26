8d4e18c8e9d25a597cd8a7dc802f78ffbba7bbba
kimbugyeong <sa01023@naver.com>
2023-03-26 20:25:57 +0900

    [FIX]챌린지조회입력방식수정
    Former-commit-id: a65fdd861a8099733f96c48bcd97c3522af07573

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ef83d1002054f3070dd1ef35bf3daca61c3942681c6066e814b2453e33dd714
+oid sha256:618ed73c0af96b90562229ac83eac432fa026aa45ee337f3e6339bc9c4b82d8a
 size 4120
