304f2e5d6cc499f3a42a2ef322a153e868e99167
이아름 <souk0712@naver.com>
2023-03-28 02:31:21 +0900

    [FEAT] 기부 캠페인 아이템 entity 생성 (#S08P22D210-202)
    Former-commit-id: 241126c0d64d94c2fbf10045ec8cf83840e6a0b5

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/campaign/CampaignCard.kt
@@ -0,0 +1,11 @@
+package com.ssafy.challenmungs.domain.entity.campaign
+
+data class CampaignCard(
+    val bannerUrl: String,
+    val title: String,
+    val charity: String,
+    val cheerCount: Int,
+    val progress: Int,
+    val target: Int,
+    val achievement: Int
+)
