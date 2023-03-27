805510c5e3d4f4bbfc7621ceaf055abf705eb587
변윤경 <forever4410@gmail.com>
2023-03-27 17:47:12 +0900

    [FEAT] 기부한 캠페인 목록 조회 API 생성
    Former-commit-id: 62525f072727bae26b4a5630ec828fe5402321ce

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/RankVo.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/RankVo.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:afc85cae1de9f82c5d7facee5e8293aa545fd8e3f7af533c1224cf632445c67e
-size 228
+package com.ssafy.ChallenMungs.challenge.panel.handler;
+
+import lombok.*;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RankVo {
+    Integer teamId;
+    Integer PanelCount;
+    Integer teamRank;
+}
--- src/main/java/com/ssafy/ChallenMungs/image/service/FileService.java
+++ src/main/java/com/ssafy/ChallenMungs/image/service/FileService.java
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7c6638e9c6eb6c876eb0cd606ffee3b650fd39ed20e8538d5a9413515dfe5a0b
-size 253
+package com.ssafy.ChallenMungs.image.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface FileService {
+    public String saveFile(MultipartFile file,String folderName) throws IOException;
+
+
+}
