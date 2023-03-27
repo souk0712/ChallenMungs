a50e9fa34aa2af5dbd6d7acc842639a9c5eca370
이동하 <siganshoyou@gmail.com>
2023-03-28 00:59:56 +0900

    [FIX]안쓰는메서드삭제
    Former-commit-id: 748516b25d255fa2884cd00ebef73228af3593a2

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/repository/GeneralBoardRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/repository/GeneralBoardRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ed3a434b5ae4028d778223f5d6536a2c82740cd50b9c6c82f566eb9b3c2b0f5
-size 1177
+oid sha256:8503cda938fc9126e4df299745316efc4dbfed9643bc545f89aa894f39d119a0
+size 1179
