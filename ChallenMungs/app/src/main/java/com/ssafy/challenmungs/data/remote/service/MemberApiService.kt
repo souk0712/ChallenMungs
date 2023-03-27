278d3914fc24b933142fb78b54ef153da68a1ccf
dobbi030 <dmstjs7047@hanmail.net>
2023-03-27 15:53:05 +0900

    [FEAT] 일반 회원 정보 얻어오는 api 변경에 따른 코드 추가
    응답에 회원의 아이디가 추가됨
    
    
    Former-commit-id: 710e421c9beeddf713adb6ad071a135ad394a61a

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/service/MemberApiService.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/service/MemberApiService.kt
@@ -1,10 +1,3 @@
-package com.ssafy.challenmungs.data.remote.service
-
-import com.ssafy.challenmungs.data.remote.datasource.member.MemberResponse
-import retrofit2.http.GET
-
-interface MemberApiService {
-
-    @GET("/user/tokenConfirm/getNameAndProfile")
-    suspend fun getMemberInfo(): MemberResponse
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a57eff5476438c4c49e47eef82596579f385e5e60f62a09c6bc42fc9bdb9dd1a
+size 292
