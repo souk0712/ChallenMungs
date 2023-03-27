33e8a936cd43c7485c4ab7e70056eb759e26915b
choijiwon <gwonwon1201@gmail.com>
2023-03-27 14:41:31 +0900

    [REFACTOR] 실패시 에러 반환하도록 변경
    Former-commit-id: 7815885c54f44e51a3992d1634c5d9a6e2fd02e1

--- src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignContentController.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/controller/CampaignContentController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64592d2eebe9c14956f9d5ea7dfb5dacffbd556f9a297cc35a4db183b5a46077
-size 3876
+oid sha256:a1ca4d6114317c743af13c807c991e09ee12eb67006b02afbabd64712d07c1c7
+size 3953
