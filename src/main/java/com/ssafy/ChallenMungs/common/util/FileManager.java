b025f561a605955b29ec300de33dc9fabf25d047
이동하 <siganshoyou@gmail.com>
2023-03-28 00:29:31 +0900

    [FIX]null6
    Former-commit-id: c25706a5aba35eac4706691ef9e4d29890648da0

--- src/main/java/com/ssafy/ChallenMungs/common/util/FileManager.java
+++ src/main/java/com/ssafy/ChallenMungs/common/util/FileManager.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d05aba2101dced6f86c16395110a62c9dbb76d8865602acfe0951bf14a5213be
-size 2510
+oid sha256:2a792225e71ea7dc70e4a07b4d6b0c9077af441c9d8499b24720cdfd03e9e974
+size 2370
