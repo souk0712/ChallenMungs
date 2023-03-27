44676d713bd111686f274f1d713ed89603d10be5
kimbugyeong <sa01023@naver.com>
2023-03-27 13:53:50 +0900

    [TEST]토큰
    Former-commit-id: 2a174b911db425ccda94d2969c57fca51a4e4d7d

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d76ac91ed95d0c67993ebe640e419908b69e3f052d058066d7c89190c149a073
-size 3347
+oid sha256:e360ecbb94f148645a44987e30d4c86a514dd2652ef0bd72e203739dbfb724d0
+size 3327
