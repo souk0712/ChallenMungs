f3da3a1c7f2c6295202d7aac935ccea8237de10a
choijiwon <gwonwon1201@gmail.com>
2023-03-27 14:27:53 +0900

    [FEAT] 월렛 수정,기부테이블 추가
    Former-commit-id: 8f30dc2187dcbb4021ac21dd306a641750665061

--- src/main/java/com/ssafy/ChallenMungs/ChallenMungsApplication.java
+++ src/main/java/com/ssafy/ChallenMungs/ChallenMungsApplication.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0559a4c026466b45a79889487f81a09fd28f366cd0c00f7b1ee7c561b55eee98
-size 441
+oid sha256:76c02dbc57b8b58b388674318c470c0f112c78e32dae10985837dcbfe72e7ae2
+size 440
