cd83372147dadb0cc27ad9ca684aa17754cacb2e
kimbugyeong <sa01023@naver.com>
2023-03-27 22:47:09 +0900

    [FEAT]인증에닉네임추가
    Former-commit-id: c262737599babfee7ec2a594d2486568bdeac3f9

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:923890feeaf4342c5f5d9e1f1017f4be53291af3127c6813538aa713a9fcad90
-size 739
+oid sha256:bfb88b632c3d7004c8819012fe9534a78f59dcf6d6385c09f5617e8d4f10cea2
+size 860
