37a39f323c3f5afa9fb822b21c7439d642d948de
kimbugyeong <sa01023@naver.com>
2023-03-27 16:30:40 +0900

    [FIX]일반챌린지게시판ENTITY수정
    Former-commit-id: beec8d9e21c39575a089a0dc24922b9bf92dbbf6

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5c09a34f68b375e6baaeb1a1b1c4369634a43ad1c4221b8f0bdad88fcfcdae2
-size 712
+oid sha256:923890feeaf4342c5f5d9e1f1017f4be53291af3127c6813538aa713a9fcad90
+size 739
