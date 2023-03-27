d997084374f854ec66c26817f2bb710e0bb16b43
kimbugyeong <sa01023@naver.com>
2023-03-27 22:51:11 +0900

    [FIX]닉네임entity수정
    Former-commit-id: ca6449c30a3119d981b775bb6eda4d3a4df9be60

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/entity/GeneralBoard.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfb88b632c3d7004c8819012fe9534a78f59dcf6d6385c09f5617e8d4f10cea2
+oid sha256:751b70d394de0abf7cd330489a89ab3283246165db0a0a29876b692ea35620f9
 size 860
