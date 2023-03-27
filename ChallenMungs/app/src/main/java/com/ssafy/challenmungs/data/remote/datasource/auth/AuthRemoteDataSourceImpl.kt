2f036ebffdb5e7756c011181cf5436fa4333707d
dobbi030 <dmstjs7047@hanmail.net>
2023-03-27 15:55:03 +0900

    [FEAT] 응답이 결과 문자열 하나로만 들어오는 api에 공통으로 사용 될 Response data class 생성
    Former-commit-id: ab99d9a2f290ccef2a2cce4290178510b8c682e9

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/auth/AuthRemoteDataSourceImpl.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/auth/AuthRemoteDataSourceImpl.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73946a3ef8ac65c177c8164afda68c8009d4a7aea6e4862d19912add5d7ac8c3
-size 831
+oid sha256:fcad849f5a6368081696270516181d3782290c90d9939419f00d7fad81a78b90
+size 833
