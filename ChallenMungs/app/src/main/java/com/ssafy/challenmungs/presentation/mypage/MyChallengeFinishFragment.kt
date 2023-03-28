da71cec7efebb9a8dd756c457e186e7bfc385200
이아름 <souk0712@naver.com>
2023-03-29 01:09:53 +0900

    [FEAT] 내 챌린지 layout 구현 일부 (#S08P22D210-271)
    Former-commit-id: a66351b1009259fa2f55bc4eff382a01bcceb92e

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyChallengeFinishFragment.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ec536e4f0b3f652e43925d4b9dcb08aa670be876a51c1a2987892bfb7f4e01ce
+size 443
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyChallengeListAdapter.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9cf4cbba10ac19cc88228ece7f3a22f07e7f2b882fc539ad6fc51d4abe097bbc
+size 483
