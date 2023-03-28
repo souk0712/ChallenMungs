b743ce1f54a0c674349f4c0cfbc5b3801132e6e1
이아름 <souk0712@naver.com>
2023-03-28 23:30:46 +0900

    [FEAT] 판넬뒤집기 생성 layout 구현 (#S08P22D210-282)
    Former-commit-id: ec7dd32c5739bccf874a2321fa97dc09600c634a

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
@@ -4,7 +4,7 @@ import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentPanelCreateBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
 
-class CreatePanelFragment :
+class PanelCreateFragment :
     BaseFragment<FragmentPanelCreateBinding>(R.layout.fragment_panel_create) {
 
     override fun initView() {
