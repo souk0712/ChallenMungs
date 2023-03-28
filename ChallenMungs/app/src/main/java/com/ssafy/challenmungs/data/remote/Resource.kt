e04299f8fd086e426bf73af3382f2c904d063769
이아름 <souk0712@naver.com>
2023-03-28 23:30:46 +0900

    [FEAT] 판넬뒤집기 생성 layout 구현 (#S08P22D210-282)
    Former-commit-id: 1bb8028f23f5f1ebcdff499baf7aff4193b0dd76

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/Resource.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/Resource.kt
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9414c6fb8adabc124cd85c3150a85ed8acc8c257c70ed353026ff82ee9063307
-size 203
+package com.ssafy.challenmungs.data.remote
+
+sealed class Resource<out T> {
+    class Success<out T>(val data: T) : Resource<T>()
+    class Error<out T>(val errorMessage: String? = null) : Resource<T>()
+}
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:75e0c77a7486dbadbc41928f8a9f8564f9dcb0d5d2bfa26bd701184416a8b451
-size 364
+package com.ssafy.challenmungs.presentation.challenge
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentPanelCreateBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class PanelCreateFragment :
+    BaseFragment<FragmentPanelCreateBinding>(R.layout.fragment_panel_create) {
+
+    override fun initView() {
+
+    }
+}
