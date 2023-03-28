abdfd04ceeacde57077653302c0dbe7b28a2d497
이아름 <souk0712@naver.com>
2023-03-28 13:26:17 +0900

    [CHORE] mr 반영 (#S08P22D210-202)
    Former-commit-id: 56ce789fb172326f933d1abd21d6f264d5d8422f

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyCheerCampaignAdapter.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/MyCheerCampaignAdapter.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e3592c9242c544f7d0a5cf9dedf196ac9577777fef6121222d505e18e904c29
-size 1260
+oid sha256:abaaf38bf957c31bbea982b18d231d4c0c2ad12969f06e3a440d17c59a83c6d6
+size 1261
