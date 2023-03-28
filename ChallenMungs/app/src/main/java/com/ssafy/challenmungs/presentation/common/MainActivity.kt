b45c6becd2cce19c99fb07dab36cf24519c7928b
dobbi030 <dmstjs7047@hanmail.net>
2023-03-28 17:54:41 +0900

    [FEAT] 스플래시 화면 이후 navigation 화면 이동 구현
    Former-commit-id: 436d2da680b53fc35ab3085c1704a02c302e4466

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/MainActivity.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/MainActivity.kt
@@ -1,8 +1,3 @@
-package com.ssafy.challenmungs.presentation.common
-
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:dbef95242999d6e9d13a42d904cf468f61828b0e4e6f62ba8e511861997254f9
+size 643
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70ec55cf638fdc1bdf2715d293fbf19d9ae000ab2a136881eae41bfca5e37dbb
-size 1556
+oid sha256:b0430c66f0c0eaa8be847fe6be3783b991da12af91f8e851a948f6cd1d23a791
+size 1400
