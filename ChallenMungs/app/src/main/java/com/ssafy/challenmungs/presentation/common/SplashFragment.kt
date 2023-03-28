7e8fd01b603baf289b20579dfe6608c0b7fd6a04
dobbi030 <dmstjs7047@hanmail.net>
2023-03-28 13:22:47 +0900

    [REFACTOR] SplashActivity를 Fragment로 변경
    MainActivity에 연결 예정
    
    
    Former-commit-id: 732764ac93145b21990867ba9696da61573f875f

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:70ec55cf638fdc1bdf2715d293fbf19d9ae000ab2a136881eae41bfca5e37dbb
+size 1556
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/splash/SplashActivity.kt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:af9e5033e1a6a769dd8a69e7e3a3cf15fcfbaadcec5700c30c87dcad7160f966
-size 1815
