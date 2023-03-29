41b92ff6b98affb830a1039dc9f2479da44f11cd
이동하 <siganshoyou@gmail.com>
2023-03-29 21:56:31 +0900

    [FEAT]12시에클레이보내는트리거추가
    Former-commit-id: 4cf8ee5dde0d67f3eb8ac13b5cca15b9a0439245

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46e743784ba16f58699098a447cfe15d9b52a52c2dcadf27dc475aeaf0816819
-size 13356
+oid sha256:45985aa10c846db13661795c36d6ce9dd2ed906043ae3f7c98412cb031197837
+size 15340
