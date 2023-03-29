1f11306ee1ab1836db70dfd1f453ea262a13cae6
dobbi030 <dmstjs7047@hanmail.net>
2023-03-29 17:23:56 +0900

    [REFACTOR] activity로의 이동을 navigate를 통해 하던 것을 intent로 변경
    Former-commit-id: 41e24e85f035044a9b8a7df40279fe01f72c75dc

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc8c6903717cf0607d91912990f8051e5fa523ac527d6fa87fcbaf955ee8537b
-size 1860
+oid sha256:56451e7f14b2eb3bafb25186923fa49055ff804b1ce48ae8feda2886daa9ecb2
+size 2098
