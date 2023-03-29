0752302f76aa66d96136a0d6da0b3d942c0d2032
이동하 <siganshoyou@gmail.com>
2023-03-30 08:13:43 +0900

    [FIX]RankVo에obtainKaly삭제
    Former-commit-id: 600961e2751f95f8cd63314bba0b221215af8a4e

--- src/main/java/com/ssafy/ChallenMungs/challenge/treasure/handler/RankVo.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/treasure/handler/RankVo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:501df4a841b61774ef6e6c21ea26f419bb167c220efcefb12e8b667d26adcbbd
-size 339
+oid sha256:6ca5fce9d695194a656e4d92423a71d12a64c4fd0dfd154c584bc12838a2bfdf
+size 315
