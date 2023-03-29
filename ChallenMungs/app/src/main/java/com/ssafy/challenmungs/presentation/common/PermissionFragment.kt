26a967ecd9331f21b93b2250536eb4d5223334dc
dobbi030 <dmstjs7047@hanmail.net>
2023-03-29 14:02:33 +0900

    [FEAT] 특정 fragment에서 뒤로가기를 두번 누르면 어플이 종료되는 기능 추가
    Former-commit-id: 608df0537f1c5885642ed13b42f6ca7b01d76eb3

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/PermissionFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/PermissionFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:807745cf8fec8f9b57658232bdb65f6f7f1a01f9d43386b41fb39772087bd817
-size 1847
+oid sha256:da399e6942b666b9e67df7566c44da027ee8539c3d6b45857758b96c46710c8d
+size 1634
