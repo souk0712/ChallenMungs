66b9d5dbaa69761003ebad277cbd981fcd53eed1
dobbi030 <dmstjs7047@hanmail.net>
2023-03-29 23:21:15 +0900

    [CHORE] 코드 컨벤션에 따른 수정
    Former-commit-id: 758ac931947a5418b9cbe468e4c5fbc70c5126ce

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/interceptor/WalletAuthInterceptor.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/interceptor/WalletAuthInterceptor.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fb49cc20d0cafc98b727a6bf8bcdf62b217afbec9ced009a3ba559680f633b5
-size 523
+oid sha256:c6e7589ae3fbbced3155824f92e8f5b20fa53f2cd30622a2fd8f317ff6945332
+size 522
