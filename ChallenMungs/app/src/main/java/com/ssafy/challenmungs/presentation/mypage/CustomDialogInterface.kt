b8ee26b6563e5daebd970567443db43294fe8606
이아름 <souk0712@naver.com>
2023-03-29 22:13:02 +0900

    [FEAT] custom Dialog layout 구현 (#S08P22D210-255)
    Former-commit-id: 9a9185704f1bed6216a70399c73f0c380375f89f

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/CustomDialogInterface.kt
@@ -0,0 +1,5 @@
+package com.ssafy.challenmungs.presentation.mypage
+
+interface CustomDialogInterface {
+    fun onPositiveButton()
+}
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/mypage/SimpleCustomDialog.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8da659d7da08ddfd5ecacc9c7b78beb11e86228bf39251cdf691ef0b4673ad7
+size 1290
