8d3ddb6ff4fbc77f2dd445f66bc1da6f9c690a78
dobbi030 <dmstjs7047@hanmail.net>
2023-03-29 17:33:34 +0900

    [FIX] 자동 로그인 결과에 따라 HomeActivity와 AuthActivity로 분기하여 이동하는 코드의 오류 수정
    true와 false 모두 HomeActivity로 이동하도록 구현했던 것을 false일 때는 AuthActivity로 변경
    
    
    Former-commit-id: 9269089a70f54b4734bdd668886ed1b1712b5367

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56451e7f14b2eb3bafb25186923fa49055ff804b1ce48ae8feda2886daa9ecb2
-size 2098
+oid sha256:04f6cd856d59b6498d8b413562cc40ea0adf7fa015ddd60cbd6611f92f22c0d5
+size 2159
