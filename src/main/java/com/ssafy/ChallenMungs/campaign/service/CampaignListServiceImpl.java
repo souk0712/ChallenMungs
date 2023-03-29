9db3f16cda71d839d91fd0b2ec3899bdd8c381b6
choijiwon <gwonwon1201@gmail.com>
2023-03-29 10:20:10 +0900

    [FEAT] 검색 추가, json 형식 바꿈
    Former-commit-id: 22f36428c7df66a63e7612769db1dfac7c6fe0a5

--- src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignListServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignListServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81f47aa356b6e8a4302efa339eead3a08fc9401e25777b45fba6ad3160f657f2
-size 7105
+oid sha256:d20d91158e2fcc9db652525534baeb37caf3102d474c4f83f572970989a2eb3d
+size 7500
