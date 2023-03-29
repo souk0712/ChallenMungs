7e8bf31d1a61d7e90fb3c483dedd3c8abec4c793
dobbi030 <dmstjs7047@hanmail.net>
2023-03-30 03:59:23 +0900

    [FEAT] 뒤로가기를 하거나 EditText가 아닌 부분을 눌렀을 때 EditText의 focus가 해제되는 기능 추가
    뒤로가기를 눌렀을 때 상하단 바로 인해 뒤로가기 버튼이 인식되지 않아 focus가 해제되지 않는 문제가 있음
    
    
    Former-commit-id: 16b494a0762e075ee1d52b26819e77b0144ccb93

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c0e3642122da74bc025e86503c682e1a14e38b66cc2327453969afdba43e5d
-size 2483
+oid sha256:c1dc932f91c39d5c9e5f9db434b3141f5fb6baff955b4bf35c668bb6204dd2b0
+size 3735
