0f3b92f89b327a22701f702ae782d4613d5c024f
이아름 <souk0712@naver.com>
2023-03-29 09:30:12 +0900

    [CHORE] @AndroidEntryPoint 추가 (#S08P22D210-271)
    Former-commit-id: be5f9919bc69692f71ca723ffe03da8576032f23

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/PanelCreateFragment.kt
@@ -3,7 +3,9 @@ package com.ssafy.challenmungs.presentation.challenge
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentPanelCreateBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PanelCreateFragment :
     BaseFragment<FragmentPanelCreateBinding>(R.layout.fragment_panel_create) {
 
