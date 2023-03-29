d77501a695ebe025bb30cff2379ae7caa8cab6b5
dobbi030 <dmstjs7047@hanmail.net>
2023-03-29 14:02:33 +0900

    [FEAT] 특정 fragment에서 뒤로가기를 두번 누르면 어플이 종료되는 기능 추가
    Former-commit-id: 0f012629a4a85abe22bfb302f2b9430547f9f9af

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/common/SplashFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0430c66f0c0eaa8be847fe6be3783b991da12af91f8e851a948f6cd1d23a791
-size 1400
+oid sha256:ad2ec358e04d34569873c7c8df2d5e788698bd6b9f07baf20e80feea2a8ed8dc
+size 1860
