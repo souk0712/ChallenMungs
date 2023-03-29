a0dca79630499e109f99adce38278da62bb3b764
kimbugyeong <sa01023@naver.com>
2023-03-29 14:35:11 +0900

    [FIX]유저반려상태저장
    Former-commit-id: 5d1feb55d06fd7283c8e352121f1608966b031ac

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f905a8d94ebb35eeb07a38ff31cfdc543808551b8395855444e6547f28d61f1d
-size 7502
+oid sha256:31cfffe60d2b16f8f8db33be079f0a9f2b99c8f87eea8e6a465a85aca7e405f2
+size 7872
