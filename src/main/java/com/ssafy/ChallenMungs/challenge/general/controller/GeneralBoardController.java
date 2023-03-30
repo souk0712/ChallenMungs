bb2adfe70171c8e9d9211b8da00a2d7a3edd83f1
kimbugyeong <sa01023@naver.com>
2023-03-30 17:38:26 +0900

    [FEAT]일반챌린지결과
    Former-commit-id: d8d811121d45cb092f9f217d7b7a400cc88a775c

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ac9ffff7225415bb7dad8826af61d3d79574e16f3e6ce86bfb04d14953a9203
-size 11360
+oid sha256:fec5bf0f756f1d7f8240f337757ff151c6cd7a00dffa707c6213263f068a3fd8
+size 11264
