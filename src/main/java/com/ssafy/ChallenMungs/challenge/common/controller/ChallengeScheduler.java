0b836584316bba197942978578d78fe08235e245
이동하 <siganshoyou@gmail.com>
2023-03-30 09:48:48 +0900

    [FEAT]클레이갯수Decimal로캐스팅
    Former-commit-id: 0085956c30b193e8337911a9cfd353dc07ed52c1

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:693d0f1613f8db2fdc312fc972829bb22df5328ea38b1c293662d44c7719f352
-size 15689
+oid sha256:826689560acabe1303fffcf7be7b68f0a6e12620f6fbfc34021285f5dcaef668
+size 15771
