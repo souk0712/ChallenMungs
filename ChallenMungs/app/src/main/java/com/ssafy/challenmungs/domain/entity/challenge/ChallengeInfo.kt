1f58330ded50160db021f7c4f023a8478c1b3863
이아름 <souk0712@naver.com>
2023-03-31 03:43:41 +0900

    [FEAT] 판넬뒤집기 참가화면 layout 구현 (#S08P22D210-278)
    Former-commit-id: 8b9d577b4258cd24f82f5b1a941259e190525d88

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/ChallengeInfo.kt
@@ -0,0 +1,14 @@
+package com.ssafy.challenmungs.domain.entity.challenge
+
+data class ChallengeInfo(
+    val title: String,
+    val category:String,
+    val startDate: String,
+    val endDate: String,
+    val fee: String,
+    val type: String,
+    val centerLat: String,
+    val centerLng: String,
+    val maxParticipantCount: Int,
+    val currentParticipantCount: Int
+)
