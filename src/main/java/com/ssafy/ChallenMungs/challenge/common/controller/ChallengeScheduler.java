a9f14a6864863e00fe7d2c05cc48bcc57bc61ffc
kimbugyeong <sa01023@naver.com>
2023-03-30 17:13:33 +0900

    [FIX]스케줄러일반챌린지
    Former-commit-id: 1de81665f33996eb5d1438d24736ff23475272b9

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9e32dc767d2ab603d5317eff271f8cdce87041df902b7a98aaf4fa6e2f1e462
-size 18377
+oid sha256:f46e601fd3f77a82441cc32810fea7269149aef142f0789bfa5cc02148c4868e
+size 18463
