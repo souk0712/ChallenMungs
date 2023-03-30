8e945a085d6afa1b5cb6c9472abf877ad654ee88
이아름 <souk0712@naver.com>
2023-03-31 03:52:24 +0900

    [CHORE] toolbar에 dataBinding 적용으로 인한 title 넣는 코드 수정
    Former-commit-id: c3ffe8b438ff1813d8a899a417bc9246e9baf382

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/OnBoardingFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68c0e3642122da74bc025e86503c682e1a14e38b66cc2327453969afdba43e5d
-size 2483
+oid sha256:458b4181003f163d675a60cbee847bb5a4593912e0f77f07a3f56e2d10cb9d21
+size 2476
