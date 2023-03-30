d91acafc72a5dad84f34c774746c3dafceed4807
변윤경 <forever4410@gmail.com>
2023-03-30 16:31:24 +0900

    [FEAT] 특별 탤린지 종료 시 승자 저금통에 보상금 입금
    Former-commit-id: e3a741437789d7c2dcec680a0d6831e4dddb44ce

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfb15800f4f5cb8269b7f226bedeaa1d5b8b363696c47638b21a027322148bd5
-size 9222
+oid sha256:3acbc4c014fe9b1b0557eff1abdc306cbca169577d92cd276e4ea8ffbe4c55e4
+size 9187
