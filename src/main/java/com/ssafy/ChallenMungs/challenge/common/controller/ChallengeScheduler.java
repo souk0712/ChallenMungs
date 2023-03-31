b141dfd3fe94ad3a8173e4ce7ee554ae973e0750
kimbugyeong <sa01023@naver.com>
2023-03-31 15:02:55 +0900

    [FEAT]일반챌린지스케줄러지갑연결값수정
    Former-commit-id: f5a59bf013b5b5f375265c9b75a3d2f1118a70b7

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e47c667f38ec9da89d717c828bf65ff16f06afbed641cc5e16f0d6d5674c5f4f
-size 19115
+oid sha256:ffc1ee5d71c3c3050d7f8e03ee0f7a20d880b837db09c29f2b3b15f7cb009871
+size 19645
