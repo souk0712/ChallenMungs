89b0ba082513e00b697008bb8d2d848a35ebd179
kimbugyeong <sa01023@naver.com>
2023-03-31 16:46:56 +0900

    [FEAT]스케줄러수정
    Former-commit-id: a18757dc2071b1ebc997f042f8d4189570cf90a0

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2006dd786022fcb82b81e1c694939954d457a332e4daf03befe39c5d25a0a74
-size 19392
+oid sha256:23b0eaa4635014539d863d10c9632c52d255d02184ef4e3c5a5835e42aa5d0c8
+size 19914
