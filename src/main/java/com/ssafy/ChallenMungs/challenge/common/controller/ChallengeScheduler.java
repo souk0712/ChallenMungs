910ec6429c25c064929acf33242d50d8213b8b77
변윤경 <forever4410@gmail.com>
2023-03-31 15:12:00 +0900

    [FEAT] 기부 누적 금액 API
    Former-commit-id: f860df5dcf5a5c95414e37a170c4605f33a699c2

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e47c667f38ec9da89d717c828bf65ff16f06afbed641cc5e16f0d6d5674c5f4f
-size 19115
+oid sha256:3e9aae85cfd05b80aa16a74027397d6a904d1667ffa3f08c113d21eea30e7b29
+size 20622
