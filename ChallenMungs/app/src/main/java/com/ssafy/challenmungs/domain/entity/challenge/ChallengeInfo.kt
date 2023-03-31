0542cb3f000ead924d1cc6c72a5e46128af7d64a
이아름 <souk0712@naver.com>
2023-03-31 15:44:36 +0900

    [CHORE] recyclerView에 databinding 적용 (#S08P22D210-278)
    Former-commit-id: e87daa70a834456d3ffb440025a2422dd419606c

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/ChallengeInfo.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/ChallengeInfo.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7629df1064ccdf8b1eb30e2db569e0041b2ad70a6dce90c7050097f48e402bd4
-size 387
+oid sha256:771db488eb67201c3718417940e579a2a2c9c3f7506b047b2e3b713a54802e1b
+size 398
