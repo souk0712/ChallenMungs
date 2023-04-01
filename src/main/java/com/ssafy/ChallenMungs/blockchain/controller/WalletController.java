7758cb51e98bd820b5916baa5fe4821c9ed49fc5
변윤경 <forever4410@gmail.com>
2023-04-01 14:59:00 +0900

    [REFACTOR] 누적 기부액 json 반환
    Former-commit-id: 774e5aa2b6e8f730cc85f04ac7db6408824de6c1

--- src/main/java/com/ssafy/ChallenMungs/blockchain/controller/WalletController.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/controller/WalletController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94737b185a1bfe5694a1a48dc539cb419dfe75f3789d70ec97e267cc0f3b9a8d
-size 4613
+oid sha256:51a3780a2d1fc7c1a09c2abc41d670be7631ddecbc76eb611c1fde739760e559
+size 4632
