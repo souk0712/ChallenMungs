d6c5c1c15c130109f47144cea0aa66087bd3826b
이동하 <siganshoyou@gmail.com>
2023-04-01 20:43:53 +0900

    [FEAT]챌린지리스트를구할때lat,lng을따로안주면거리제한상관않고나타나요
    Former-commit-id: 779d2a1e080dfa67155fd90ceb41a86a0742ad9e

--- src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignContentService.java
+++ src/main/java/com/ssafy/ChallenMungs/campaign/service/CampaignContentService.java
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:57fcb2ff3f28619fb7444b16b04a3119879cc2f2c4e00aa20783bd66953c5e28
-size 434
+package com.ssafy.ChallenMungs.campaign.service;
+
+import com.ssafy.ChallenMungs.campaign.dto.CampaignDetailDto;
+import com.ssafy.ChallenMungs.campaign.dto.CampaignInsertDto;
+
+public interface CampaignContentService {
+    void createCampaign(CampaignInsertDto info);
+    boolean isCampaignAble(String loginId);
+
+    int cheerUpCampaign(String loginId, int campaignId);
+    CampaignDetailDto viewDetailCampaign(int campaignId);
+
+
+
+
+
+
+}
--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6889f7438a595d4d48e70702e2991a5adfe9a01e50ebb723db69eda92fde53a
-size 14923
+oid sha256:dd15d1ad0e952a4e7296bb653a6fa87242ce8aa37f2f7c7dd7e980b4a7b99f44
+size 15103
