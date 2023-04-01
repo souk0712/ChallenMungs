2fb9020b81138cdd60a531673cee7f519ff2dca3
이아름 <souk0712@naver.com>
2023-04-01 21:30:28 +0900

    [FEAT] 판넬 뒤집기 방 정보 가져오기 일부 구현 (#S08P22D210-277)
    Former-commit-id: e69f51ab980e858bb34270fe60efba6da9f7c9bb

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/usecase/challenge/GetChallengeInfoUseCase.kt
@@ -0,0 +1,10 @@
+package com.ssafy.challenmungs.domain.usecase.challenge
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class GetChallengeInfoUseCase @Inject constructor(
+
+){
+}
