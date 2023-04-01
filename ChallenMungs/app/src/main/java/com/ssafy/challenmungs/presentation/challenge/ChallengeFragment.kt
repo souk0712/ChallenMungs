72ca2ba6bc89e3d1bed3e8ddd9525d5391b4cde2
dobbi030 <dmstjs7047@hanmail.net>
2023-04-01 16:42:21 +0900

    [FEAT] 챌린지 홈 화면 layout 구현
    Former-commit-id: 326f3ab058a36987e7085b5a1c6a4c84cec04aa1

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
@@ -1,12 +1,3 @@
-package com.ssafy.challenmungs.presentation.challenge
-
-import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.databinding.FragmentChallengeBinding
-import com.ssafy.challenmungs.presentation.base.BaseFragment
-
-class ChallengeFragment : BaseFragment<FragmentChallengeBinding>(R.layout.fragment_challenge) {
-
-    override fun initView() {
-
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:64026ea4a1bb5bb2e1509e5f42bad4cfde2eca77bc493fcf9c86e1a853f305d4
+size 697
