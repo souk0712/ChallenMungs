998cb83aabfc21644a5f3e1e01a373517e435c6f
dobbi030 <dmstjs7047@hanmail.net>
2023-04-01 14:20:34 +0900

    [FEAT] 챌린지 홈 화면 layout 기본 구현
    recyclerview 추가 구현 필요
    
    
    Former-commit-id: c88f6672403aac875ebc5ecba5cfac775e07b0fb

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.challenmungs.presentation.challenge
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentChallengeBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class ChallengeFragment : BaseFragment<FragmentChallengeBinding>(R.layout.fragment_challenge) {
+
+    override fun initView() {
+
+    }
+}
