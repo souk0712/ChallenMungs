3403aab645cfde50feb71e9582cbc617207ff0f6
변윤경 <forever4410@gmail.com>
2023-04-01 14:53:40 +0900

    [REFACTOR] wallet API tokenConfirm 적용
    Former-commit-id: 068c4908b083cd45b60013dd6d9fa02f99d1cd6a

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletService.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a00e5b7334248ac708e055515bebf4b08678d97d2b0c240a6ce9da4facc5d6a
-size 1128
+oid sha256:a510edc9a0b942c70d1dc05ecbb344287e6fd6e42a71af54d1a1168701639614
+size 1139
