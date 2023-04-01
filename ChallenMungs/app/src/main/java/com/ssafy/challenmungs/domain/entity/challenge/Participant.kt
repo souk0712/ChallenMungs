e61f42798aeaca3ab4d5a459be5b884a36f3bcec
이아름 <souk0712@naver.com>
2023-04-01 14:57:39 +0900

    [SETTING] WebSocket 서버 주소 추가
    Former-commit-id: 29cadf9f9a56e9d2e1b23ad6c079a8ff7e6af2b8

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/Participant.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/Participant.kt
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d44204084ed1fcbe7fd0ffd8c616ffdecb162d885ba07a6867941883e1b58f4a
-size 138
+package com.ssafy.challenmungs.domain.entity.challenge
+
+data class Participant(
+    val profileImageUrl: String?,
+    val name: String,
+)
