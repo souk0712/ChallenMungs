9481bdeee86f9581c2df7d7798e3a43e4ae036f9
이동하 <siganshoyou@gmail.com>
2023-04-01 21:13:28 +0900

    [FEAT]시작여부와상관없이다가져오는대신현재진행중인것만거르는코드뺌
    Former-commit-id: bd69ec5f5ae23364bc5ae64f1a799014627c35eb

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd15d1ad0e952a4e7296bb653a6fa87242ce8aa37f2f7c7dd7e980b4a7b99f44
-size 15103
+oid sha256:9e8201cf14a2bed66956a69c0806f891cf9ef887acc2246f8fb1ac66737b26ec
+size 14914
