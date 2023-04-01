df8189480c10fd1a1ba9f089c66f3d1516125af7
김부경 <sa01023@naver.com>
2023-04-01 21:07:43 +0900

    [FIX]출금계좌로직수정
    Former-commit-id: 2a02ac34be4f3c26a740d3bf1b02a3efd25d8c7e

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/RankVo.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/RankVo.java
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:52e7a80406ba0697bdde8a348c99520b6349e078fe9df9510e77ce0a46204217
-size 277
+package com.ssafy.ChallenMungs.challenge.panel.handler;
+
+import lombok.*;
+
+import java.util.ArrayList;
+
+@Getter@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RankVo {
+    Integer teamId;
+    Integer PanelCount;
+    Integer teamRank;
+    Object loginId;
+}
--- src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
+++ src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef65262c99063c3d655161d63ff512be608c77f0971fba4972c8e6f705f0d1f
-size 16807
+oid sha256:4d69b931b53123d85777ef7f5525dddf13eb3c4ec12a589433164a7ad0d3ce3f
+size 16892
