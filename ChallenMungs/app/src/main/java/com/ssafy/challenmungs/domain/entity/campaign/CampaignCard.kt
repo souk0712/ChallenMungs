6e75419d2ba08a1aa03654a862a286e1f52d589d
이동하 <siganshoyou@gmail.com>
2023-04-02 22:17:21 +0900

    정보화면에핀을꼽았어요
    Former-commit-id: 9bf53d4a1ac4e064bb491cc57385ffa40d1416a4

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/campaign/CampaignCard.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/campaign/CampaignCard.kt
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8db361f9ded81066e605ca6b561ed69a5f87e912558ce3b7cdeda04da5ec10e8
-size 254
+package com.ssafy.challenmungs.domain.entity.campaign
+
+data class CampaignCard(
+    val bannerUrl: String,
+    val title: String,
+    val charity: String,
+    val cheerCount: String,
+    val progress: Int,
+    val target: Int,
+    val achievement: Int
+)
