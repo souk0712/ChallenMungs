a56611aebd675dd5efe700d0eaea153cb6ce4c6f
이동하 <siganshoyou@gmail.com>
2023-04-02 14:57:28 +0900

    [FEAT]캠페인정보불러올때로그인아이디추가
    Former-commit-id: f3e94a682638ec97acc623acefac5c42342744d5

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:894f988f446f2959fd61cadcd50c40717bbd416187471ff4106cea5fd6f20acb
-size 22212
+oid sha256:ae653c234275ee1b75efdaa46d0b0fd3d25ecf4922af9b9926fd7d2c8f1f5640
+size 22437
