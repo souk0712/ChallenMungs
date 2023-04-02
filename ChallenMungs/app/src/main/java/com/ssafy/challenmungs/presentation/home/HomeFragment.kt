943fb07ac1a2d285f76d2333b41c2e28c34b194d
이동하 <siganshoyou@gmail.com>
2023-04-02 20:21:07 +0900

    [FEAT]정보탭시작하고홈프레그먼트에빈칸도지웠어요
    Former-commit-id: 6af0f05ea50426c46fa0077bac845bb4f846ab1f

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/HomeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/HomeFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a58ddfb6e5e15732bc53cf02777b93531564dc655def4c18add54afedcccc954
-size 8002
+oid sha256:c6aa0bee65f0ed730246043339da8e5e29c67471ebcfc047cc8344de94cd2933
+size 7953
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/information/InformationFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/information/InformationFragment.kt
@@ -1,9 +1,3 @@
-package com.ssafy.challenmungs.presentation.information
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.ssafy.challenmungs.R
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:08102648eb6b41c033cccb1299f422c38db155339950512d078846300f6cc386
+size 575
