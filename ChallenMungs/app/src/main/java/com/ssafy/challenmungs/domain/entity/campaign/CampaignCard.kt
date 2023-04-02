491595230fe736fc2dce0d111e0a8d95c86679ac
이동하 <siganshoyou@gmail.com>
2023-04-02 17:41:32 +0900

    [FEAT]홈화면됐음
    Former-commit-id: d97745427754745d9bbe1c80910fc3726557a62b

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/campaign/CampaignCard.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/campaign/CampaignCard.kt
@@ -1,11 +1,3 @@
-package com.ssafy.challenmungs.domain.entity.campaign
-
-data class CampaignCard(
-    val bannerUrl: String,
-    val title: String,
-    val charity: String,
-    val cheerCount: Int,
-    val progress: Int,
-    val target: Int,
-    val achievement: Int
-)
+version https://git-lfs.github.com/spec/v1
+oid sha256:8db361f9ded81066e605ca6b561ed69a5f87e912558ce3b7cdeda04da5ec10e8
+size 254
