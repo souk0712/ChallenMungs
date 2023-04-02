c8c23a8b9ede27733e7516e564373d7336a3b7a7
이동하 <siganshoyou@gmail.com>
2023-04-02 20:24:03 +0900

    [FIX]진짜다고침
    Former-commit-id: a076b6561bdb7515908089395fd32ec3d5823d97

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/MyChallengeListViewHolder.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/MyChallengeListViewHolder.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8883fec75ce0d368a1840162db035db284771f02fed28c1bbf6c83db1bb1489d
-size 1386
+oid sha256:b1d1551e189f5b91000b4075a0992f0f9543e845ab074e201c12abbfbde095fa
+size 1391
