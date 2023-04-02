a931698dfbfd0d8d8ff83407f89d110297513fa3
dobbi030 <dmstjs7047@hanmail.net>
2023-04-02 14:14:24 +0900

    [FIX] /wallet/normal api의 주소가 변경되어 오류가 발생하던 문제 해결
    api를 /wallet/tokenConfirm/normal로 변경 후 header에 token을 넣어서 전달하는 memberAPI로 이동
    
    
    Former-commit-id: edb304d4cd224803ff32d0c8e2cef264aca920e2

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/auth/AuthRemoteDataSource.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/auth/AuthRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.ssafy.challenmungs.data.remote.datasource.auth
 
-import com.ssafy.challenmungs.data.remote.datasource.common.ResultResponse
 import okhttp3.RequestBody
 
 interface AuthRemoteDataSource {
@@ -8,6 +7,4 @@ interface AuthRemoteDataSource {
     suspend fun requestLogin(body: RequestBody): LogInResponse
 
     suspend fun requestJoin(name: String, accessToken: String): JoinResponse
-
-    suspend fun setWallet(memberId: String, piggyBank: String, wallet: String): ResultResponse
 }
