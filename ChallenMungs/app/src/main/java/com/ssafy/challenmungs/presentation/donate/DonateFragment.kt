09ced94bb99ab6183cd53075f883b29d2360abad
dobbi030 <dmstjs7047@hanmail.net>
2023-04-02 20:12:20 +0900

    [FEAT] 캠페인 기부 페이지 layout 구현
    Former-commit-id: 350fc1290619e1e4d51c383822f256892833e2c7

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/donate/DonateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/donate/DonateFragment.kt
@@ -3,7 +3,9 @@ package com.ssafy.challenmungs.presentation.donate
 import com.ssafy.challenmungs.R
 import com.ssafy.challenmungs.databinding.FragmentDonateBinding
 import com.ssafy.challenmungs.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DonateFragment: BaseFragment<FragmentDonateBinding>(R.layout.fragment_donate) {
 
     override fun initView() {
