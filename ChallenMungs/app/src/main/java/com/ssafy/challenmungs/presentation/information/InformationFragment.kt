da17df730e34a3217980f18646248dd80d7b366e
이동하 <siganshoyou@gmail.com>
2023-04-02 19:29:47 +0900

    [FEAT]빈줄지우기
    Former-commit-id: 0d3df5c3e9f7984be8e98b4490c4cac0ad34f11b

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/information/InformationFragment.kt
@@ -0,0 +1,9 @@
+package com.ssafy.challenmungs.presentation.information
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ssafy.challenmungs.R
+
