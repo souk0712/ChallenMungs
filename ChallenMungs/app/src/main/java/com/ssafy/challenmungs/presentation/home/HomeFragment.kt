a5727af9ce01483f36f2fe8f8bc3c972d821e7cc
이동하 <siganshoyou@gmail.com>
2023-04-02 18:07:04 +0900

    [FIX]코멘트검토
    Former-commit-id: fcb34674c829ad77a194cf3e0e1191b2ce126b46

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/HomeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/HomeFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d4b056749f4773edefa5aa3ef946d7837c2a1fff1ea589a088b078a4c6b9641
-size 8251
+oid sha256:392d820892831ff3f243d225c82fcba3114279f9a69895d8b3bfbaf80d43eeaf
+size 8006
