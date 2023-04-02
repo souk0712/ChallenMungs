d4e1f6e26dbe44bb8ff00e53d98ed81badbae1cf
dobbi030 <dmstjs7047@hanmail.net>
2023-04-02 14:14:24 +0900

    [FIX] /wallet/normal api의 주소가 변경되어 오류가 발생하던 문제 해결
    api를 /wallet/tokenConfirm/normal로 변경 후 header에 token을 넣어서 전달하는 memberAPI로 이동
    
    
    Former-commit-id: ee57e42b1c82b1e8a39959b6d7a8ff501f6d915e

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/member/MemberRemoteDataSource.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/member/MemberRemoteDataSource.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0b45eb1f3258ae0d91971f9b311a53c9ebc7f987ade941f82b5a575a4fb5494
-size 147
+oid sha256:9bb3ae13c913c1114f7f19faae8f624a883c4b90542bcff7dc034673ae690b08
+size 319
