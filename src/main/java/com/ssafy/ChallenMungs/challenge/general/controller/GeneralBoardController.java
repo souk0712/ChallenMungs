cd4d9c947ff366d93563c86b2a6b1b5f235fee35
kimbugyeong <sa01023@naver.com>
2023-04-03 14:46:34 +0900

    [FIX]지갑연결값수정
    Former-commit-id: f7ce7c29a768af0a079014e75f88a9655ad63d26

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fec5bf0f756f1d7f8240f337757ff151c6cd7a00dffa707c6213263f068a3fd8
-size 11264
+oid sha256:72ec551c24baf714a8cc44d55d921b3054a5d81e5f641e4f01cbd0c3243cce6a
+size 11445
