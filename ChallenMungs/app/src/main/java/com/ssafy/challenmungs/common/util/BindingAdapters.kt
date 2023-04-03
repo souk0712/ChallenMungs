6ed8c15ee69e71178a91ca6ea2ccd2d9eca3dde6
dobbi030 <dmstjs7047@hanmail.net>
2023-04-04 03:37:33 +0900

    [REFACTOR] campaign banner image의 기본 사진 변경
    Former-commit-id: 7acc5a271c58a7fa25c5b23bcdc924f4fb296152

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/BindingAdapters.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/BindingAdapters.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a283d82cdb3aea272f4d0e4d8d7f9fbde1ffee1cf112896389c8964231ea6b8
-size 2420
+oid sha256:169a04fd098dcfaae92c320de04b12c92c0725d6721b274f6a1e5d5d9d533d44
+size 2511
