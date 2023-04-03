f2326eb9970236a36083508dd3250f45f54431d5
이동하 <siganshoyou@gmail.com>
2023-04-03 13:59:50 +0900

    [FEAT]판넬정보불러오는API에LAT,LNG정봋추가
    Former-commit-id: 33e91fc145c15447ee7ca700b8b52e8429367d53

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a0eaca6a1fd56744e95f2beafc01bca896e05fd8d633d3bf361ac619dae1347
-size 6109
+oid sha256:33d38d46a1180b4e3633e609a9afa01f3b35a7b0162ec28d3235ab78af9c6660
+size 6227
