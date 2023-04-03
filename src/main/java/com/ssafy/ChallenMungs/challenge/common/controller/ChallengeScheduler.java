bf18de48b1eed716aec15797dc102d50c21a0537
이동하 <siganshoyou@gmail.com>
2023-04-03 23:15:36 +0900

    [FEAT]가로길이수정
    Former-commit-id: 1d70dda6ae360995d1cf4f7b8fd51e0856fe88e7

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa27f453831d622994fb6da5bd0082760fa3c3f3ecdf70f9b4ccd1528871e11a
-size 25518
+oid sha256:23ee8f67389d4e1a011685e39fa8f056e6f99a8471a419a06a4fb27ab187971d
+size 25583
