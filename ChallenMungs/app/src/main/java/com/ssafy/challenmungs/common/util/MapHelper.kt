592d852fdea8b1c4ea37f48d4199fee1d90f9f83
이아름 <souk0712@naver.com>
2023-04-03 23:14:40 +0900

    [CHORE] replaceDrawableMarker 함수 이미지 크기 조정
    Former-commit-id: 2f09a43bc9d1ff5b2f5ca8c2d6543f682e347204

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5691aedb3f136caa5c8e88fc10a7dc03de143a3f69355fb9e251d1f327a0ada
-size 2505
+oid sha256:bc32262813d952d993c8d78264ad4389f326a660d956167027fb8d268115c53e
+size 2680
