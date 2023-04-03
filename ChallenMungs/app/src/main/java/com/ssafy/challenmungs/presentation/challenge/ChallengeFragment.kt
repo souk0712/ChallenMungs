38f90900d2b9af9ce2ac7944ca8e83969b6f55a8
dobbi030 <dmstjs7047@hanmail.net>
2023-04-03 17:24:27 +0900

    [FEAT] recyclerview에 아이템 연결
    Former-commit-id: e5ec39fbbac3fb0d95e6c06150329d02e46070e5

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7883f9bbc24b618b565e0da8a6a4640890465a8a027b6c87dbd0352e26ee2866
-size 837
+oid sha256:de5f95251692cf120983fd607b89262185cc180f990ae8dc3f9ff07c6e0cafbb
+size 1760
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeListAdapter.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/ChallengeListAdapter.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f358f57719a3f929e142d04fc16753ec712ad482f9001c16aacc9989ac69d2b8
-size 1590
+oid sha256:244de113dd59dc8d7686e7a356038d3c6e89d7c20b223c978abd05f1cefdbaff
+size 2585
