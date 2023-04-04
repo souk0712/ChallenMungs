7b876e11af1a685a1122fde8a9b5800e216d1b22
dobbi030 <dmstjs7047@hanmail.net>
2023-04-05 02:11:31 +0900

    [FEAT] viewpager2 구현
    Former-commit-id: 75e892393d3fda9feaccc2181b918c994cb8b5e4

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/basic/ChallengeBasicHistoryFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.challenmungs.presentation.challenge.basic
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentChallengeBasicHistoryBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class ChallengeBasicHistoryFragment :
+    BaseFragment<FragmentChallengeBasicHistoryBinding>(R.layout.fragment_challenge_basic_history) {
+
+    override fun initView() {}
+}
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/basic/ChallengeBasicTodayFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.challenmungs.presentation.challenge.basic
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentChallengeBasicTodayBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class ChallengeBasicTodayFragment :
+    BaseFragment<FragmentChallengeBasicTodayBinding>(R.layout.fragment_challenge_basic_today) {
+
+    override fun initView() {}
+}
