c3b757f7e8edeaf0dc791db334618f0100e4f6a2
이아름 <souk0712@naver.com>
2023-04-05 01:47:08 +0900

    [CHORE] string 수정 및 추가
    Former-commit-id: e5618866111b8d712b4c65fcc3fa1d40690218fe

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:031d93ddc8f8fe41ad8f9014670a97d35b9eb65f892eb59c582a9df47f88ff62
-size 15185
+oid sha256:44eac1b594d2446e844c9c41fd0428ce4b6282cba1d75df3dd1c3597a29e610a
+size 15213
