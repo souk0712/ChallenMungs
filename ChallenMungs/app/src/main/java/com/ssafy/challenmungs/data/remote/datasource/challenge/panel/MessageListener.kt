65246168274e8adfd6893b045a26422e57e68ded
이아름 <souk0712@naver.com>
2023-04-04 09:45:49 +0900

    [FEAT] 웹소켓 관련 구현
    Former-commit-id: 8adaa376161ec215498cced6e18aeb31c5d30b9e

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/challenge/panel/MessageListener.kt
@@ -0,0 +1,9 @@
+package com.ssafy.challenmungs.data.remote.datasource.challenge.panel
+
+interface MessageListener {
+
+    fun onConnectSuccess()
+    fun onConnectFailed()
+    fun onClose()
+    fun onMessage(text: String?)
+}
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/challenge/panel/WebSocketManager.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5723a9523e609ba4d16977bf010203bf13fde050e738b2b5fbc71c2a9a9022ea
+size 3468
