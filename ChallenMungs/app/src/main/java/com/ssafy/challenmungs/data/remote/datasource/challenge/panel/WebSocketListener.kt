834dee037ca438df8efb158f15408b5689e90f0f
이아름 <souk0712@naver.com>
2023-04-04 09:45:49 +0900

    [FEAT] 웹소켓 관련 구현
    Former-commit-id: 9e0b67581244e923f12b0146b73d16b7413787e3

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/data/remote/datasource/challenge/panel/WebSocketListener.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d6348ad1ba16252f13431aae7faea73380ff792cbe9ea1edbcfa9baaf50f285
+size 1511
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/WebSocketData.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a26830666fdfb4a69e73201bee43720c8400c4631a73aa84022c0ee2b887377
+size 815
