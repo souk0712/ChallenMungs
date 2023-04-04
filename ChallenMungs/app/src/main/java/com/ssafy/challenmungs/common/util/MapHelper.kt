b77c40a6b77adb3a93964486f33e764b5a4f5148
이아름 <souk0712@naver.com>
2023-04-04 09:41:45 +0900

    [FEAT] 마지막위치 반환하는 함수와 팀에 따라 판넬 색 반환하는 함수 구현
    Former-commit-id: ce986c01f7b21690a36f044f3acc9a75c54e59a5

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/MapHelper.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc32262813d952d993c8d78264ad4389f326a660d956167027fb8d268115c53e
-size 2680
+oid sha256:3c1b169cc9e4176dac9c952250d55bca1210f5beb464f2a243619afd7db85d09
+size 3740
