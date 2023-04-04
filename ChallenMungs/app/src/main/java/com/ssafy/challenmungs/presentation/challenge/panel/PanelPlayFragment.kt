53987ad9b59ea38f193e42baf4fc17b74b129422
이아름 <souk0712@naver.com>
2023-04-05 05:05:02 +0900

    [CHORE] 머지 충돌 해결
    Former-commit-id: cb888dd251b34afbcfb8cb43d918984f0f3d76cd

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44eac1b594d2446e844c9c41fd0428ce4b6282cba1d75df3dd1c3597a29e610a
-size 15213
+oid sha256:a969fbafe9e95a8a8ee4001795b877bd08293c351192a62c313a52019dcc57f4
+size 15136
