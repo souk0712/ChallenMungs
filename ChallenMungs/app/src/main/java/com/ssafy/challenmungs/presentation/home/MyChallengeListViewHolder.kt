4fde7c6628ac0042cc358d7d3201f2c1ef0b8860
이동하 <siganshoyou@gmail.com>
2023-04-05 04:15:09 +0900

    [FEAT]6장만들고지도기능다구현
    Former-commit-id: d9d755be68a5715b03c4366fbd141cc62685b2e0

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/MyChallengeListViewHolder.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/home/MyChallengeListViewHolder.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d1551e189f5b91000b4075a0992f0f9543e845ab074e201c12abbfbde095fa
-size 1391
+oid sha256:aaa5b7f8c44b9c84dd0f971db8844b112689a582f40b336cf42ff31a95316f93
+size 1520
---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/information/InformationPlaceListFragment.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0ecd970bcc081680a7eb8fa2ebd549fa5e7040316b6792c6523a6d070422d980
+size 1773
