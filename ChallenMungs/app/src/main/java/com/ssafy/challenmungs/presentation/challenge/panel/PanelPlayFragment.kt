9f87efc15ff927971fb649c596243214010093b1
이아름 <souk0712@naver.com>
2023-04-04 12:31:45 +0900

    [FEAT] 정보 페이지 연결 및 데이터 넣기
    Former-commit-id: 5c65cf48a2329b911b33aa3b7bad5599d0314402

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae69d0b1643b9bf3bacbf59d28e0276a454db9269c214b4392555e71a630d603
-size 15019
+oid sha256:031d93ddc8f8fe41ad8f9014670a97d35b9eb65f892eb59c582a9df47f88ff62
+size 15185
