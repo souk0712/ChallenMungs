5cc61c065cd47b134c6980478a052bdc13153c0f
이동하 <siganshoyou@gmail.com>
2023-04-05 03:20:04 +0900

    [FIX]리줄트추가
    Former-commit-id: f413ee169d20647fb843052ccfc777edf47c3bb2

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a6f44bf0204280e49288a9c85481883f64cecec9329f0426b1e836220b04a2f
-size 7668
+oid sha256:9aabea22d05c649970cbe074e9d4ccfc1dc18dae54d40c7f26f7e7f70f2ef92d
+size 7755
