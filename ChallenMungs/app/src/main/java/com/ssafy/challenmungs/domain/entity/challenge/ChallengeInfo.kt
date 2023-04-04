6c9da2f6fdeca476dc84e377478a9d04ef712f16
이아름 <souk0712@naver.com>
2023-04-04 09:01:49 +0900

    [CHORE] 웹소켓 데이터 타입에 따라 수정
    Former-commit-id: 7de0e8dec86008d559d00f88dd9a9875bc12faab

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/ChallengeInfo.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/ChallengeInfo.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d3294edeb77047c89d4c25b86c6125be3cccf704c422376db7403221b6e540f
-size 726
+oid sha256:dd728f9c2869f6273744624396bf0b5c1dfccab84000749227dd5529054a791c
+size 719
