33bedda08ceb33308c123a4c60bf8312ebfda84a
dobbi030 <dmstjs7047@hanmail.net>
2023-04-05 12:56:47 +0900

    [CHORE] git convention에 따른 수정
    Former-commit-id: 5b7a70f99fcf59dc7cc96b986c7b93c08cc8b257

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/basic/ChallengeBasicFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/basic/ChallengeBasicFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a982d41d039df236b90468a7f120690e1853319e58e8dabd8bff4589e698da51
-size 2112
+oid sha256:1cf382d89f4e640dfd646d55dfcf266fd0d8ca0ed622f7d98d24b8a2787c59dd
+size 2099
