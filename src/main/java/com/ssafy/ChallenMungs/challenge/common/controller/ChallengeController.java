3ad70accaa8287c1d372cef2d05d21b46bb657b3
이동하 <siganshoyou@gmail.com>
2023-04-06 02:28:10 +0900

    [FEAT]겟인포
    Former-commit-id: 52cb8a92b498e70aa4546ccde9a236a643905563

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bebc8c5c5ee82cf2a995fe5a21aff61e704793638c1720fcbc7fb58f3d7048b9
-size 17063
+oid sha256:445c082899139822ad86ca9ee9c4fbfe39f6c97f2b59f40c540e759453ac2507
+size 18291
