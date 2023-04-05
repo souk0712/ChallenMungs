b67e6ba007bc6c86fdc20fa4eacf4c35af614690
dobbi030 <dmstjs7047@hanmail.net>
2023-04-05 21:40:43 +0900

    [FEAT] deprecated 된 함수에 Suppress 어노테이션 붙이기
    Former-commit-id: 50ff3d4858d711e428125061a34b82dc1a24d663

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/base/BaseFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/base/BaseFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed3e7878f9786bcc528aed9bec823171df3fbf74b698de6812eda48afe6631b7
-size 4967
+oid sha256:a0c6f82389e6c986ac3f50a5ac9524d3dfab1ef77154015f4e26a7c2af85705f
+size 4996
