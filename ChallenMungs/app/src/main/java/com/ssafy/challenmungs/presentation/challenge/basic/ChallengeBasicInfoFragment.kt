a77b301a37c8a041eea6046dee5cec36ed9428bf
dobbi030 <dmstjs7047@hanmail.net>
2023-04-06 00:25:50 +0900

    [FEAT] 시작하지 않은 챌린지 카드 클릭시 fragment 이동
    Former-commit-id: 16691ffafd7d66b0ff767fd43029483be1440f68

---
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/basic/ChallengeBasicInfoFragment.kt
@@ -0,0 +1,11 @@
+package com.ssafy.challenmungs.presentation.challenge.basic
+
+import com.ssafy.challenmungs.R
+import com.ssafy.challenmungs.databinding.FragmentChallengeBasicInfoBinding
+import com.ssafy.challenmungs.presentation.base.BaseFragment
+
+class ChallengeBasicInfoFragment :
+    BaseFragment<FragmentChallengeBasicInfoBinding>(R.layout.fragment_challenge_basic_info) {
+
+    override fun initView() {}
+}
