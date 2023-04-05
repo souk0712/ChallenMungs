6cd70126ef095b08e0f3f959e3ce76a8e275271f
이아름 <souk0712@naver.com>
2023-04-05 13:06:23 +0900

    [CHORE] api 변경에 따른 구현 및 databinding 연결
    Former-commit-id: 27bcc1dcb5bcd885dfc860d7ce6d30b2340e7359

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayViewModel.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayViewModel.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb206c0f6ea67b1542e72b8f9a0d242d843ca3caf0c8e0174c58c919e4025dff
-size 3408
+oid sha256:f328fffdc3bcda9e724a5b28591e4f4c3da2109729f774b253c28a44fbf34f31
+size 3955
