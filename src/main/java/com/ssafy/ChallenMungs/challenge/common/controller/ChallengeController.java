31cca28bb3bc1e07890eec28e8979b2686504b1e
이동하 <siganshoyou@gmail.com>
2023-04-06 02:52:37 +0900

    [FEAT]겟인챌린지분리
    Former-commit-id: 3c6acfc03d8661bb07701ff43d30c4f3ccf9e69a

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:445c082899139822ad86ca9ee9c4fbfe39f6c97f2b59f40c540e759453ac2507
-size 18291
+oid sha256:56431bd6612ae81b8ac3f68a5eb66d935d21544062ecb07b1e21ebb862e8b513
+size 17517
