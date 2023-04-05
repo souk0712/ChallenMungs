4363dbaac8834b37c5f977be6c1cc8fddad4debb
변윤경 <forever4410@gmail.com>
2023-04-05 09:30:15 +0900

    [REFACOTR] 판넬 챌린지 생성시 클레이튼 전송
    Former-commit-id: 230d9a5e6ba49d60d0e13106f1fdf3eca1cdb2af

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b98d5a1992bf7555918548a8ef0240b980c2aa2404704f42f490e0aee2d6eb96
-size 7501
+oid sha256:7fb18e441a07f348766c6a4b698f64c4bd9fca2bf1f9973d1c1251abd72d5e3a
+size 8098
