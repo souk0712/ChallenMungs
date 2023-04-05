bd2c7d23c14527335825c7304def79a3b680502d
이아름 <souk0712@naver.com>
2023-04-05 13:05:20 +0900

    [CHORE] api에 따른 변수 추가 및 변경
    Former-commit-id: 99a7b7c37bae196fe8d51a86cabb4a3f83808ce4

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/WebSocketData.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/domain/entity/challenge/WebSocketData.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a26830666fdfb4a69e73201bee43720c8400c4631a73aa84022c0ee2b887377
-size 815
+oid sha256:bc317c4bb46c4cd5aa15688cf99046c72e5ccfe661d9b5f5764c055c80b2516b
+size 842
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelPlayFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a969fbafe9e95a8a8ee4001795b877bd08293c351192a62c313a52019dcc57f4
-size 15136
+oid sha256:f5b62b70add48ed79422119a03f9647efa7a93c6f35166a1fa4521684786545e
+size 15077
