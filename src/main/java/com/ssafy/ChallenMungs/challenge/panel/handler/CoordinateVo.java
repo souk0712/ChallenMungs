219f9a29c9a90eebab293e5c70e14adcf45db455
kimbugyeong <sa01023@naver.com>
2023-04-05 10:56:47 +0900

    [FIX]스케줄러챌린지참가취소시금액변동
    Former-commit-id: 2630d6f5ac49b954c8e3dd071cdc8d45a3333e2d

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/CoordinateVo.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/handler/CoordinateVo.java
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3d63eb930888ba07d6c0098b12296236264faf7c429883af06ab5ab5f3635296
-size 201
+package com.ssafy.ChallenMungs.challenge.panel.handler;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CoordinateVo {
+    Double lat;
+    Double lng;
+}
