f240ba170cceb56129b8ff4206299fc63bdb05fe
이아름 <souk0712@naver.com>
2023-04-05 13:04:14 +0900

    [FIX] PlaySettingDialog에서 간헐적으로 앱이 종료되는 에러 해결
    context를 붙이기 전에 요청하여 생기는 문제로 추정
    context를 전달하는 방향으로 수정.
    
    
    Former-commit-id: 8ea8b0cd6591a7401785060daa105027b84740f4

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelCreateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelCreateFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e22c440540527a515332c2f3f20f8bcd9b85f564e9caf1b9d77d15e14d0b980e
-size 12043
+oid sha256:17bbe5bbe4f2824319fd07d7890ac45f16215bc9f9a29bd0daa6da6ed1a1b9ed
+size 12061
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PlayAreaSettingDialog.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PlayAreaSettingDialog.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7303aff4ab96cc2bcdfe1effe9bb25282ea09b4af65644e116fb8d52e0eff9b
-size 6889
+oid sha256:eca978db9ce1abcc1fefc51d4117aba3f3eedbd728af0213d47af859ba67ced2
+size 6986
