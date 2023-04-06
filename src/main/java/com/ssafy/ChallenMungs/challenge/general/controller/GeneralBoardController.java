6b1cbc72a1e0d75a8a05df60ff24d8acb56b2ded
kimbugyeong <sa01023@naver.com>
2023-04-07 01:08:04 +0900

    [FIX]인증게시글한국시간설정
    Former-commit-id: 693e92ab44c9b9fd2f2239133a2a8723cfd35b1c

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aa34b42d1be5d01a49c66dfb0b2d23c4d3d1ce7b65b4c9b43ee23304c4dc2e0
-size 12298
+oid sha256:5691306ed32f99c86a695cd5f4fbb2521ed34237c7382ff487adbecdbb8a4f27
+size 12346
