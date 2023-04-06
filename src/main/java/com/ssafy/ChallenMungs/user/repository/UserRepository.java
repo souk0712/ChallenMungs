1ad6d551b01dcb402d3c802103236220960482c2
변윤경 <forever4410@gmail.com>
2023-04-07 00:20:12 +0900

    [FIX] 아름이의 잘못된 계좌
    Former-commit-id: 33342d0ebc793e6aead8381dd457798966c87c3f

--- src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
+++ src/main/java/com/ssafy/ChallenMungs/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de12283cf72a907a224afea89b96f59119b37cdaae02519ed876f09789bf8de5
-size 712
+oid sha256:4f12ac66f9aed35e7c5ebdeacd52547832a78c6b10d9b97e3ddb7eab5730c669
+size 772
