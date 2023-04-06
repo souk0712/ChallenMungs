48df4b99bf9b5bb270703b69b52aca7ab7377596
변윤경 <forever4410@gmail.com>
2023-04-06 09:54:00 +0900

    [REFACTOR] 잔액 정수로 반환
    Former-commit-id: 3ebca40441b6c27bfccbc000781ae504b202b9be

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e7f860f0e6e65ba1198b794f7e5720ea81465a9b112d3b4d669db1cfb5072de
-size 17044
+oid sha256:b3017b00f99f8ee19d7a9928b69fc52577c9807cc1b5d2740b2e51f54fc77fe1
+size 17043
--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4c07cdb3b6f9a75174c97f27d80bdfa88e7b9806b1988812559ff7b7f365a3e
-size 28079
+oid sha256:9084c57a91bf7f8f950ce4a726bc00a0f5ef67e345d9fcddb7397a2569696c6b
+size 27842
