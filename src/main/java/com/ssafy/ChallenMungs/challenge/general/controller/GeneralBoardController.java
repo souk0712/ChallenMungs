275f0eeab629e3e5654ae581361633c37134d79c
kimbugyeong <sa01023@naver.com>
2023-04-06 23:22:55 +0900

    [FIX]인증게시물이없을때빈배열반환
    Former-commit-id: e588752ca20052beda65223b1104a8154abaf25b

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:406d07e6eeaedf07ed3ac281ec95bd6c57e419aa09fa7b757f93ba4dde761ca5
-size 11798
+oid sha256:1c0e0fffaed9e972b0c58c6fe5139958cabc0f790ee38d9bce9a292a9d8b1c2b
+size 11789
