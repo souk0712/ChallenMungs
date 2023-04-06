d11b807e96a82f8289142cb28014608d67c817ab
이아름 <souk0712@naver.com>
2023-04-07 02:44:51 +0900

    [FEAT] 판넬 챌린지 생성 이동 처리
    Former-commit-id: b91430d0dd7f48241beaaacd558dff5246606b5e

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelCreateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/challenge/panel/PanelCreateFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b101b0857351d064235e84085661d65e01a503794d1d5db08466bea483b34781
-size 12031
+oid sha256:eaeb2e4a94ee2ae6fce2fab4a94c326ac48751937c86a2314fd33d1ae1b7359c
+size 12319
