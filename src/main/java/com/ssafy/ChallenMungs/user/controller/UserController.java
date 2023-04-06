fe5150d8dddf702454fcb010463496df56472e69
이동하 <siganshoyou@gmail.com>
2023-04-06 21:20:51 +0900

    [FEAT]기본이미지
    Former-commit-id: 219cb68fcd6596b0e6a118476f62492d80431376

--- src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
+++ src/main/java/com/ssafy/ChallenMungs/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d69b931b53123d85777ef7f5525dddf13eb3c4ec12a589433164a7ad0d3ce3f
-size 16892
+oid sha256:de2f628adedcf5b214f417c3c1ffb4b8a821eb06f125ee8b6c83d29121376b81
+size 17031
