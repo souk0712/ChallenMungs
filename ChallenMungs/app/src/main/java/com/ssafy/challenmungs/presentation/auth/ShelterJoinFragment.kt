976f6918f03550f497fdc061bd3b6c1706b165cf
이아름 <souk0712@naver.com>
2023-04-07 02:43:58 +0900

    [FEAT] 마이페이지 네비게이션 추가
    Former-commit-id: 3e7c2ee8b272e2295140273c865e354c39a71e3b

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/ShelterJoinFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/ShelterJoinFragment.kt
@@ -1,13 +1,3 @@
-package com.ssafy.challenmungs.presentation.auth
-
-import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.databinding.FragmentShelterJoinBinding
-import com.ssafy.challenmungs.presentation.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class ShelterJoinFragment :
-    BaseFragment<FragmentShelterJoinBinding>(R.layout.fragment_shelter_join) {
-
-    override fun initView() {}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:07a85dec3b68b1859d03be91903e47c25e9d611e3d0caa8ba8deceee79544330
+size 417
