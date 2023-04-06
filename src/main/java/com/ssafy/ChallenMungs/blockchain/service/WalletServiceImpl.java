90f5cd5ae6ff546177c9dc1ef187fb3e2ddc6224
kimbugyeong <sa01023@naver.com>
2023-04-06 14:11:43 +0900

    [FIX]지갑히스토리출력
    Former-commit-id: 3f31affe9cb4a4ff72a60a0a63f18cf10e4b1e52

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletServiceImpl.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:39eee90a3de3001d10e74498d54c30a7638fe7fb73a7150ef56bbdc8d643e014
-size 17732
+oid sha256:fdd43cb82a3c07a3ea95df190fb30322d7146f0750d81c96a7a87be2c5309db8
+size 18182
