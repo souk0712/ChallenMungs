becbe2b17748c451ce4f86e027da375738055403
kimbugyeong <sa01023@naver.com>
2023-04-06 16:43:59 +0900

    [FIX]인증게시물이없을때예외처리
    Former-commit-id: 80002935a77b0017f99df3ca9fd06577de9223fb

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/entity/Challenge.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/entity/Challenge.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e17ca0085991709bae13e30b51a512ad6517edeb3ac502fb92afef3c62a562fa
+oid sha256:58f32d8947db4005a8e9f09779970843083c90c1b9bc9a9f76930e0bbafa9574
 size 1864
