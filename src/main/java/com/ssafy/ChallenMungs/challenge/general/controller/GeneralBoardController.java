396fee229e356900bd1dff33545eda8a97b133cf
kimbugyeong <sa01023@naver.com>
2023-04-06 16:50:36 +0900

    [FIX]인증게시물이없을때예외처리추가
    Former-commit-id: 0cb264cb938eec7f221fff2bb4cc7149a42c8d63

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3617d13e7911972eb6c1e3cd1ba29b76c3c8b5e1a1c30e0c41c80a53507a2506
-size 11726
+oid sha256:406d07e6eeaedf07ed3ac281ec95bd6c57e419aa09fa7b757f93ba4dde761ca5
+size 11798
