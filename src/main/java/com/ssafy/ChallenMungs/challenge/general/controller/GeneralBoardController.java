1979dc69c5e5bc2432f8025d3a44a785ed7c2558
kimbugyeong <sa01023@naver.com>
2023-04-07 02:22:26 +0900

    [FIX]성공갱신결과반환변경
    Former-commit-id: 9deaa51b3801d93eaacff2028a8ec5f2dbf0ce0a

--- src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/general/controller/GeneralBoardController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88e8a2d49aa3d7490762ea0e8926824f96064dc910dcf103901cd19da39b0421
-size 12507
+oid sha256:a17e69710c8d74de7227c7236c71f57101cf959814cfa0e07ff332df79dfa89f
+size 12664
