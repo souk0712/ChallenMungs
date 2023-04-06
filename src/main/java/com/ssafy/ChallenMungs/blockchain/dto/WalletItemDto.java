d6f167fe963dc1fbbfc4dbc0b62c86851e2f61e1
변윤경 <forever4410@gmail.com>
2023-04-07 00:23:23 +0900

    [REFACTOR] 내 지갑 amount -> money"
    Former-commit-id: 13b201e99da12bae1c7f6952ca5b63c774b58873

--- src/main/java/com/ssafy/ChallenMungs/blockchain/dto/WalletItemDto.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/dto/WalletItemDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc61fb2e19ad8ea8ec9ce9c31ac5ea21a948b04edee6b0d7c9ef50464624de25
-size 303
+oid sha256:5c0c48792dada10d945a4a3ca2d66facdc992e24eada98d247ff01dfa01cfdac
+size 302
