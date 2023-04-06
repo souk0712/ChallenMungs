ef270a60997332abd7b3430094ae8cf25ad131eb
이아름 <souk0712@naver.com>
2023-04-07 02:47:15 +0900

    [FEAT] 거래 내역 adapter 연결, ui 수정
    Former-commit-id: 9293c3ff780532512e4a81ab13728fd684108a83

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/BindingAdapters.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/common/util/BindingAdapters.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62485649229c4efe8f3d416f9a5e06e7f50a54609e0a2ff32f1a0c74a4a5a565
-size 2553
+oid sha256:ced4fb389b2f06cd69f497479f6c3b8bf071d93cd605e3272120ed8ae8e8ea1a
+size 3861
