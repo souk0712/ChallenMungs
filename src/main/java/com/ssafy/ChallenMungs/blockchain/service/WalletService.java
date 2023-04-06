dcb4f793383e98fd54d784b72bbb1a1e543841c7
kimbugyeong <sa01023@naver.com>
2023-04-06 15:43:25 +0900

    [FIX]피기뱅크출력방식수정
    Former-commit-id: 919d5f4753af2abe7f8a62facffc55aa49206698

--- src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletService.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/service/WalletService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51d4ebdb60b30ad850ae9148043be61f9fa32239b5fe51bc494f34e7e61b761b
-size 1308
+oid sha256:1729ce7786ddf5130c377b1c5a66e557a2cfc1a7d7e2bcc92bac5bd1c6c0dafa
+size 1241
