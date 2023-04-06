1c9624129e2808cf23cb864bf08ba84472d94533
변윤경 <forever4410@gmail.com>
2023-04-06 21:49:31 +0900

    [CHORE] 에러확인
    Former-commit-id: 8c127da70464aac90f963064c52ab3401cd8ec66

--- src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/panel/controller/PanelController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2e5561f42a252353df7a1c99369c2d5ebb0422d3446b158fc09a096d1aba0e8
-size 8353
+oid sha256:4871bd4c83ac2ffbb19ea480c426e5c98e33e2151645e66d4f71ec378aed1fcf
+size 8393
