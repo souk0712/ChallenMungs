3fe136a8612a4875e95eea5af7a169613152849b
kimbugyeong <sa01023@naver.com>
2023-04-06 14:44:53 +0900

    [FIX]챌린지기간정보추가
    Former-commit-id: adcaa3fa1bd48dda15ba728710f9eace4926ea05

--- src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
+++ src/main/java/com/ssafy/ChallenMungs/challenge/common/controller/ChallengeController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d4eeece2f3f8f5087b85ac9a9a84527b827f58195acfd355311db80cd7a945e
-size 18089
+oid sha256:4c2f30313810bfa6aa4237dddb8a2e46ea71c7a255c35cafe91312e9ec752af6
+size 18233
