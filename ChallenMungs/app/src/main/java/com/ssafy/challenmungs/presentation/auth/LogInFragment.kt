7a0ec2ce6b1e3ec39e6b6694eb83584662b0ac01
dobbi030 <dmstjs7047@hanmail.net>
2023-04-06 23:43:09 +0900

    [FEAT] 기부처 회원가입 기능 구현
    기부처가 초대코드를 요청하고, 받은 초대코드로 회원가입을 진행 할 수 있음. 자동로그인 구현
    
    
    Former-commit-id: 0ef38a82a44ca82ebf39fe2106a1642b42d3fd42

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/LogInFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/LogInFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e974d0433a960f5ea350eec36be2efb4aa126bb0caa281d3c391c445c35c8cb
-size 4261
+oid sha256:d68d2f803c9abf5d7f3b4c4885751740b8f1e31433b0a8edf9bdcfcc60f29f49
+size 4267
--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/ShelterJoinFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/auth/ShelterJoinFragment.kt
@@ -1,13 +1,3 @@
-package com.ssafy.challenmungs.presentation.auth
-
-import com.ssafy.challenmungs.R
-import com.ssafy.challenmungs.databinding.FragmentShelterJoinBinding
-import com.ssafy.challenmungs.presentation.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class ShelterJoinFragment :
-    BaseFragment<FragmentShelterJoinBinding>(R.layout.fragment_shelter_join) {
-
-    override fun initView() {}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:c81b942f96c05335dede08b19d54ac5d72cc850420fea561671e38f7d760ebe5
+size 2158
