7554fb2fde4bfd8c589cf32a5f8b95daec4afdaa
변윤경 <forever4410@gmail.com>
2023-04-06 23:13:44 +0900

    [REFACTOR] Donate Tokenconfirm
    Former-commit-id: 210b53f2b29177a01e1de202b1650c12ed641dae

--- src/main/java/com/ssafy/ChallenMungs/blockchain/controller/WalletController.java
+++ src/main/java/com/ssafy/ChallenMungs/blockchain/controller/WalletController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d141f2abfe30199c60f7dd42ac17894d23aa3f5dcc77726624fd178c87b1bbb
-size 5783
+oid sha256:d1232be8e3234b2d792130f3187b9b35dfc5bb731f7bcff00cd9cedf8edb27a6
+size 6135
