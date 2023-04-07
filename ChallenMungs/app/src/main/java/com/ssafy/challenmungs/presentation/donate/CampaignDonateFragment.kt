d1ec779100c43710e0ea616edeeee535e05daf7d
dobbi030 <dmstjs7047@hanmail.net>
2023-04-07 09:39:45 +0900

    [FIX] navigation id 참조 오류 해결
    Former-commit-id: c184e419e0bba96f620cc561eae950aacbb47f0b

--- ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/donate/CampaignDonateFragment.kt
+++ ChallenMungs/app/src/main/java/com/ssafy/challenmungs/presentation/donate/CampaignDonateFragment.kt
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75149bdac36fff279572cacf93b9ae5e607466363d6dfe194e37cc92c6a7b575
-size 3224
+oid sha256:0750f839d86b86702e5158d6679593a715f7313aa4f4a586a19971c24fa4c856
+size 3342
